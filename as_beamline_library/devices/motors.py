af0a2db34c4c885693ec0b4a5e5b13816d42540c
Stephen Mudie <stephenm@ansto.gov.au>
2022-02-15 23:41:07 +0000

    Updating python requirements.

---
+++ as_beamline_library/devices/motors.py
@@ -0,0 +1,6 @@
+"""
+Motor Definitions
+"""
+from .classes.test_devices import SimTable2
+
+my_table = SimTable2(name="blah_table")
---
+++ tests/motors_test.py
@@ -0,0 +1,9 @@
+from as_beamline_library.devices.motors import my_table
+
+
+def test_motor():
+    
+    test_pos_1 = 10
+    my_table.x.set(test_pos_1)
+    assert my_table.x.position == test_pos_1
+
