911d24c30bdd721d30ad6e1badf69edbdb83f31f
Stephen Mudie <stephenm@ansto.gov.au>
2022-02-25 01:56:46 +0000

    Relative imports.

--- as_beamline_library/plans/basic_scans.py
+++ as_beamline_library/plans/basic_scans.py
@@ -2,11 +2,11 @@
 
 from functools import partial
 
-import bluesky.plan_stubs as bps
+# import bluesky.plan_stubs as bps
 from bluesky.plans import scan
 
-from as_beamline_library.devices.detectors import my_detector
-from as_beamline_library.devices.motors import my_motor
+from ..devices.detectors import my_detector
+from ..devices.motors import my_motor
 
 step_shoot = partial(scan, [my_detector], my_motor)
 
