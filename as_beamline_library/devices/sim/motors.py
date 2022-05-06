174c9ceb1a8a12d7fd005afef25fc2048775daee
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2022-05-06 13:20:40 +1000

    added sim motors

--- as_beamline_library/devices/sim/motors.py
+++ as_beamline_library/devices/sim/motors.py
@@ -3,9 +3,11 @@
 from as_acquisition_library.devices.motors import ASSimMotor
 
 from .classes.motors import MySimTable
-from ophyd.sim import motor1
+from ophyd.sim import motor1, motor2, motor3
 
 my_motor = ASSimMotor(name="my_motor")
 my_table = MySimTable(name="my_table")
 
 motor_x = motor1
+motor_y = motor2
+motor_z = motor3
