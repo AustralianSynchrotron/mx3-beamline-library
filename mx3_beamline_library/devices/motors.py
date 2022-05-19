1d9f687e83c0cc4b88ff12cce0654422ca796464
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2022-05-19 11:54:07 +1000

    added mxcube simulated devices

--- mx3_beamline_library/devices/motors.py
+++ mx3_beamline_library/devices/motors.py
@@ -1,5 +1,7 @@
 """ Motor configuration and instantiation. """
 
-from .classes.motors import Testrig
+from .classes.motors import MxcubeSimulatedPVs, Testrig
 
 testrig = Testrig("MX3-testrig", name="testrig")
+
+mxcube_sim_PVs = MxcubeSimulatedPVs("MXCUBE", name="mxcube_sim_PVs")
--- mx3_beamline_library/devices/sim/classes/motors.py
+++ mx3_beamline_library/devices/sim/classes/motors.py
@@ -10,3 +10,18 @@ class MySimTable(MotorBundle):
     x = Cpt(ASSimMotor, name="AXIS:X")
     y = Cpt(ASSimMotor, name="AXIS:Y")
     z = Cpt(ASSimMotor, name="AXIS:Z")
+
+
+class SimulatedPVs(MotorBundle):
+    """
+    Simulated PVs for Mxcube
+    """
+
+    m1 = Cpt(ASSimMotor, name="MXCUBE:m1")
+    m2 = Cpt(ASSimMotor, name="MXCUBE:m2")
+    m3 = Cpt(ASSimMotor, name="MXCUBE:m3")
+    m4 = Cpt(ASSimMotor, name="MXCUBE:m4")
+    m5 = Cpt(ASSimMotor, name="MXCUBE:m5")
+    m6 = Cpt(ASSimMotor, name="MXCUBE:m6")
+    m7 = Cpt(ASSimMotor, name="MXCUBE:m7")
+    m8 = Cpt(ASSimMotor, name="MXCUBE:m8")
