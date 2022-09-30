4565c34064e8c31bffd6c9979cf7a74fed803548
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2022-09-30 15:13:49 +1000

    added grid scan

--- test_lucid.py
+++ test_lucid.py
@@ -18,12 +18,21 @@ import numpy.typing as npt
 import numpy as np
 import matplotlib.pyplot as plt
 
-def save_image(data,screen_coordinates, filename):
+def save_image(data: npt.NDArray,screen_coordinates: list, filename: str):
     plt.imshow(data) 
     plt.scatter(screen_coordinates[1], screen_coordinates[2],s=200, c="r", marker="+")
     plt.savefig(filename)
     plt.close()
 
+def take_snapshot(camera: BlackFlyCam, filename: str, screen_coordinates: list=[612, 512]):
+    array_data: npt.NDArray = camera.array_data.get()
+    data = array_data.reshape(
+        camera.height.get(), camera.width.get(), camera.depth.get()
+    )
+    plt.imshow(data) 
+    plt.scatter(screen_coordinates[0], screen_coordinates[1],s=200, c="r", marker="+")
+    plt.savefig(filename)
+    plt.close()
 
 testrig = motors.testrig
 motor_x = testrig.x
@@ -38,13 +47,13 @@ motor_phi.wait_for_connection()
 blackfly_camera = detectors.blackfly_camera
 blackfly_camera.wait_for_connection()
 
-def move_motors_to_loop_edge(motor_x: CosylabMotor, motor_z: CosylabMotor, camera: BlackFlyCam):
-    beam_position = [612, 512]
-    pixels_per_mm = [292.87, 292.87]
+dectris_detector = detectors.dectris_detector
 
+def plot_raster_grid(camera: BlackFlyCam, initial_pos_pixels: list[int], final_pos_pixels: list[int], filename):
+    
     array_data: npt.NDArray = camera.array_data.get()
     data = array_data.reshape(
-        camera.height.get(), camera.width.get(), camera.depth.get()
+        blackfly_camera.height.get(), blackfly_camera.width.get(), blackfly_camera.depth.get()
     ).astype(np.uint8)
 
     screen_coordinates = lucid3.find_loop(
@@ -52,7 +61,50 @@ def move_motors_to_loop_edge(motor_x: CosylabMotor, motor_z: CosylabMotor, camer
         rotation=True,
         rotation_k=1,
     )
-    save_image(data,screen_coordinates,f"fig_{screen_coordinates[1]}")
+    # Plot loop edge which is used as a reference to plot the grid
+    plt.scatter(screen_coordinates[1], screen_coordinates[2],s=200, c="b", marker="+")
+
+    plt.imshow(data)
+    
+    # Plot Grid limits:
+    # Top
+    x = np.linspace(initial_pos_pixels[0],final_pos_pixels[0],100)
+    z = initial_pos_pixels[1]*np.ones(len(x))
+    plt.plot(x,z, color="red", linestyle="--")
+
+    # Bottom
+    x = np.linspace(initial_pos_pixels[0],final_pos_pixels[0],100)
+    z = final_pos_pixels[1]*np.ones(len(x))
+    plt.plot(x,z, color="red", linestyle="--")
+
+    # Right side
+    z = np.linspace(initial_pos_pixels[1],final_pos_pixels[1],100)
+    x = final_pos_pixels[0]*np.ones(len(x))
+    plt.plot(x,z, color="red", linestyle="--")
+
+    # Left side
+    z = np.linspace(initial_pos_pixels[1],final_pos_pixels[1],100)
+    x = initial_pos_pixels[0]*np.ones(len(x))
+    plt.plot(x,z, color="red", linestyle="--")
+
+    plt.savefig(filename)
+    plt.close()
+
+def move_motors_to_loop_edge(motor_x: CosylabMotor, motor_z: CosylabMotor, camera: BlackFlyCam):
+    beam_position = [612, 512]
+    pixels_per_mm = [292.87, 292.87]
+
+    array_data: npt.NDArray = camera.array_data.get()
+    data = array_data.reshape(
+        camera.height.get(), camera.width.get(), camera.depth.get()
+    )
+
+    screen_coordinates = lucid3.find_loop(
+        image=data,
+        rotation=True,
+        rotation_k=1,
+    )
+    save_image(data,screen_coordinates,f"figs/fig_{screen_coordinates[1]}")
 
     loop_position_x = motor_x.position + (screen_coordinates[1] - beam_position[0]) / pixels_per_mm[0]
     loop_position_z = motor_z.position + (screen_coordinates[2] - beam_position[1]) / pixels_per_mm[1]
@@ -67,6 +119,60 @@ def optical_centering(motor_x, motor_z, motor_phi, camera):
     yield from mvr(motor_phi, 90)
     yield from move_motors_to_loop_edge(motor_x, motor_z, camera)
     yield from mvr(motor_phi, 90)
+    yield from move_motors_to_loop_edge(motor_x, motor_z, camera)
+    take_snapshot(camera, "figs/second_last_pos")
+
+    yield from mvr(motor_z, -0.6)
+
+    take_snapshot(camera, "figs/final_pos")
+
+def master_plan(motor_x: CosylabMotor, motor_z: CosylabMotor, motor_phi: CosylabMotor, camera: BlackFlyCam):
+    beam_position = [612, 512]
+    pixels_per_mm = [292.87, 292.87]
+
+    yield from optical_centering(motor_x, motor_z, motor_phi, camera)
+
+    array_data: npt.NDArray = camera.array_data.get()
+    data = array_data.reshape(
+        camera.height.get(), camera.width.get(), camera.depth.get()
+    )
+
+    screen_coordinates = lucid3.find_loop(
+        image=data,
+        rotation=True,
+        rotation_k=1,
+    )
+
+    delta_z = abs(screen_coordinates[2] - beam_position[1]) / pixels_per_mm[1]
+
+    initial_pos_pixels = [beam_position[0] - abs(screen_coordinates[2] - beam_position[1]),
+        beam_position[1] - abs(screen_coordinates[2] - beam_position[1])]
+    take_snapshot(
+        camera, "figs/initial_pos_grid",
+        initial_pos_pixels 
+    )
+    
+    final_pos_pixels = [beam_position[0] + abs(screen_coordinates[2] - beam_position[1]),
+        beam_position[1] + abs(screen_coordinates[2] - beam_position[1])]
+    take_snapshot(
+        camera, "figs/final_pos_grid",
+        final_pos_pixels
+    )
+    plot_raster_grid(camera, initial_pos_pixels, final_pos_pixels,"figs/grid")
+
+
+    initial_pos_x = motor_x.position - delta_z
+    final_pos_x = motor_x.position + delta_z
+
+    initial_pos_z = motor_z.position - delta_z
+    final_pos_z = motor_z.position + delta_z
+
+    yield from grid_scan(
+        [dectris_detector], motor_z, initial_pos_z, final_pos_z, 2, 
+        motor_x, initial_pos_x, final_pos_x, 2,
+        md={"sample_id": "test"})
+
+
 
 bec = BestEffortCallback()
 RE = RunEngine({})
@@ -81,7 +187,7 @@ RE(mv(motor_phi, 0))
 
 print("starting loop centering")
 
-RE(optical_centering(motor_x, motor_z, motor_phi, blackfly_camera))
+RE(master_plan(motor_x, motor_z, motor_phi, blackfly_camera))
 
 
 
