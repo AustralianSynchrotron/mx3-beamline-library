97f869df3e498653f561c91aa38e33dba5829aad
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2022-11-03 10:50:30 +1100

    drive motors to center of loop

--- mx3_beamline_library/plans/optical_centering.py
+++ mx3_beamline_library/plans/optical_centering.py
@@ -15,9 +15,101 @@ from mx3_beamline_library.devices.classes.detectors import BlackFlyCam
 from mx3_beamline_library.devices.classes.motors import CosylabMotor
 from mx3_beamline_library.plans.psi_optical_centering import loopImageProcessing
 
-BEAM_POSITION = ast.literal_eval(environ.get("BEAM_POSITION", "[612, 512]"))
+BEAM_POSITION = ast.literal_eval(environ.get("BEAM_POSITION", "[640, 512]"))
 PIXELS_PER_MM_X = float(environ.get("PIXELS_PER_MM_X", "292.87"))
 PIXELS_PER_MM_Z = float(environ.get("PIXELS_PER_MM_Z", "292.87"))
+
+
+def plot_raster_grid_with_center(
+    camera: BlackFlyCam,
+    rectangle_coordinates: dict,
+    screen_coordinates: tuple[int, int],
+    filename: str,
+) -> None:
+    """
+    Plots the limits of the raster grid on top of the image taken from the
+    camera.
+
+    Parameters
+    ----------
+    camera : BlackFlyCam
+        A blackfly camera
+    initial_pos_pixels : list[int, int]
+        The x and z coordinates of the initial position of the grid
+    final_pos_pixels : list[int, int]
+        The x and z coordinates of the final position of the grid
+    filename : str
+        The name of the PNG file
+
+    Returns
+    -------
+    None
+    """
+    plt.figure()
+    array_data: npt.NDArray = camera.array_data.get()
+    data = array_data.reshape(
+        camera.height.get(),
+        camera.width.get(),
+        camera.depth.get(),
+    )
+    plt.imshow(data)
+
+    # Plot grid:
+    # Top
+    plt.scatter(
+        rectangle_coordinates["top_left"][0],
+        rectangle_coordinates["top_left"][1],
+        s=200,
+        c="b",
+        marker="+",
+    )
+    plt.scatter(
+        rectangle_coordinates["bottom_right"][0],
+        rectangle_coordinates["bottom_right"][1],
+        s=200,
+        c="b",
+        marker="+",
+    )
+
+    # top
+    x = np.linspace(
+        rectangle_coordinates["top_left"][0],
+        rectangle_coordinates["bottom_right"][0],
+        100,
+    )
+    z = rectangle_coordinates["top_left"][1] * np.ones(len(x))
+    plt.plot(x, z, color="red", linestyle="--")
+
+    # Bottom
+    x = np.linspace(
+        rectangle_coordinates["top_left"][0],
+        rectangle_coordinates["bottom_right"][0],
+        100,
+    )
+    z = rectangle_coordinates["bottom_right"][1] * np.ones(len(x))
+    plt.plot(x, z, color="red", linestyle="--")
+
+    # Right side
+    z = np.linspace(
+        rectangle_coordinates["top_left"][1],
+        rectangle_coordinates["bottom_right"][1],
+        100,
+    )
+    x = rectangle_coordinates["bottom_right"][0] * np.ones(len(x))
+    plt.plot(x, z, color="red", linestyle="--")
+
+    # Left side
+    z = np.linspace(
+        rectangle_coordinates["top_left"][1],
+        rectangle_coordinates["bottom_right"][1],
+        100,
+    )
+    x = rectangle_coordinates["top_left"][0] * np.ones(len(x))
+    plt.plot(x, z, color="red", linestyle="--")
+
+    plt.scatter(screen_coordinates[0], screen_coordinates[1], s=200, c="r", marker="+")
+    plt.savefig(filename)
+    plt.close()
 
 
 def take_snapshot(
@@ -170,7 +262,7 @@ def unblur_image(
     yield from mv(focus_motor, maximum)
 
 
-def move_motors_to_loop_edge(
+def drive_motors_to_loop_edge(
     motor_x: CosylabMotor,
     motor_z: CosylabMotor,
     camera: BlackFlyCam,
@@ -240,6 +332,47 @@ def move_motors_to_loop_edge(
     yield from mv(motor_x, loop_position_x, motor_z, loop_position_z)
 
 
+def drive_motors_to_center_of_loop(
+    motor_x: CosylabMotor,
+    motor_z: CosylabMotor,
+    camera: BlackFlyCam,
+    plot: bool = False,
+):
+
+    array_data: npt.NDArray = camera.array_data.get()
+    data = array_data.reshape(
+        camera.height.get(), camera.width.get(), camera.depth.get()
+    ).astype(np.uint8)
+
+    procImg = loopImageProcessing(data)
+    procImg.findContour(zoom="-208.0", beamline="X06DA")
+    procImg.findExtremes()
+    rectangle_coordinates = procImg.fitRectangle()
+
+    pos_x_pixels = (
+        rectangle_coordinates["top_left"][0] + rectangle_coordinates["bottom_right"][0]
+    ) / 2
+    pos_z_pixels = (
+        rectangle_coordinates["top_left"][1] + rectangle_coordinates["bottom_right"][1]
+    ) / 2
+
+    if plot:
+        plot_raster_grid_with_center(
+            camera,
+            rectangle_coordinates,
+            (pos_x_pixels, pos_z_pixels),
+            "step_2_centered_loop",
+        )
+
+    loop_position_x = (
+        motor_x.position + (pos_x_pixels - BEAM_POSITION[0]) / PIXELS_PER_MM_X
+    )
+    loop_position_z = (
+        motor_z.position + (pos_z_pixels - BEAM_POSITION[1]) / PIXELS_PER_MM_Z
+    )
+    yield from mv(motor_x, loop_position_x, motor_z, loop_position_z)
+
+
 def optical_centering(
     motor_x: CosylabMotor,
     motor_y: CosylabMotor,
@@ -288,19 +421,15 @@ def optical_centering(
     Generator[Msg, None, None]
         A plan that automatically centers a loop
     """
-    yield from mv(motor_phi, 0)
-    if auto_focus:
-        yield from unblur_image(camera, motor_y, min_focus, max_focus, tol)
-    logging.info(f"------------------Omega----------: {motor_phi.position}")
-    yield from move_motors_to_loop_edge(motor_x, motor_z, camera, plot, method)
 
-    yield from mv(motor_phi, 90)
-    logging.info(f"------------------Omega----------: {motor_phi.position}")
-    yield from move_motors_to_loop_edge(motor_x, motor_z, camera, plot, method)
+    omega_list = [0, 90, 180]
+    for omega in omega_list:
+        yield from mv(motor_phi, omega)
+        logging.info(f"Omega: {motor_phi.position}")
 
-    yield from mv(motor_phi, 180)
-    logging.info(f"------------------Omega----------: {motor_phi.position}")
-    yield from move_motors_to_loop_edge(motor_x, motor_z, camera, plot, method)
+        if auto_focus and omega == 0:
+            yield from unblur_image(camera, motor_y, min_focus, max_focus, tol)
 
-    if plot:
-        take_snapshot(camera, "step_2_centered_loop")
+        yield from drive_motors_to_loop_edge(motor_x, motor_z, camera, plot, method)
+
+    yield from drive_motors_to_center_of_loop(motor_x, motor_z, camera, plot)
