f2a48bbd137f4651ee6b39bf515443b7e04bb687
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2022-11-10 15:55:22 +1100

    pre-commit

--- mx3_beamline_library/science/optical_and_loop_centering/center_of_mass.py
+++ mx3_beamline_library/science/optical_and_loop_centering/center_of_mass.py
@@ -1,11 +1,11 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
-import matplotlib.pyplot as plt
 
 
 def center_of_mass(number_of_spots: npt.NDArray) -> tuple[int, int]:
     """
-    Calculate the center of mass of a sample given an array containing 
+    Calculate the center of mass of a sample given an array containing
     the number of spots
 
     Parameters
@@ -17,10 +17,10 @@ def center_of_mass(number_of_spots: npt.NDArray) -> tuple[int, int]:
     -------
     x_cm, y_cm : tuple[int, int]
         The x and y center of mass coordinates which correspond to the x and y indeces
-        of the numpy array  
+        of the numpy array
     """
     shape = number_of_spots.shape
-    
+
     y_cm = 0
     for i in range(shape[0]):
         for j in range(shape[1]):
@@ -36,9 +36,11 @@ def center_of_mass(number_of_spots: npt.NDArray) -> tuple[int, int]:
     return x_cm, y_cm
 
 
-def multi_crystal_center_of_mass(number_of_spots: npt.NDArray, threshold: int) -> list[tuple[int,int]]:
+def multi_crystal_center_of_mass(
+    number_of_spots: npt.NDArray, threshold: int
+) -> list[tuple[int, int]]:
     """
-    Calculates the center of mass of individual crystals given an 
+    Calculates the center of mass of individual crystals given an
     array containing the numbers of spots with shape (n_rows, n_cols)
 
     Parameters
@@ -59,8 +61,8 @@ def multi_crystal_center_of_mass(number_of_spots: npt.NDArray, threshold: int) -
     island_start_index = []
     y, x = np.nonzero(number_of_spots)
     for i in range(len(x)):
-        if i>0:
-            if (y[i] - y[i-1]) >1 or (x[i] - x[i-1]) >1:
+        if i > 0:
+            if (y[i] - y[i - 1]) > 1 or (x[i] - x[i - 1]) > 1:
                 island_start_index.append(i)
 
     # calculate the center of mass of each "island"
@@ -71,13 +73,13 @@ def multi_crystal_center_of_mass(number_of_spots: npt.NDArray, threshold: int) -
             center_of_mass_list.append(center_of_mass(island))
             island = np.zeros(np.shape(number_of_spots))
 
-        island[y[i]][x[i]] = number_of_spots[y[i]][x[i]] 
+        island[y[i]][x[i]] = number_of_spots[y[i]][x[i]]
 
-        if i ==(len(x)-1):
+        if i == (len(x) - 1):
             center_of_mass_list.append(center_of_mass(island))
 
-
     return center_of_mass_list
+
 
 def filter_array(array: npt.NDArray, threshold: int) -> npt.NDArray:
     """
@@ -89,7 +91,7 @@ def filter_array(array: npt.NDArray, threshold: int) -> npt.NDArray:
     array : npt.NDArray
         A numpy array containing the number of spots
     threshold : int
-        We replace the values of the array below the threshold with zeros 
+        We replace the values of the array below the threshold with zeros
 
     Returns
     -------
@@ -103,9 +105,12 @@ def filter_array(array: npt.NDArray, threshold: int) -> npt.NDArray:
         filtered_array[tuple(arg)] = 0
     return filtered_array
 
-def plot_muti_crystal_center_of_mass(number_of_spots: npt.NDArray, threshold: int, save: bool=False):
+
+def plot_muti_crystal_center_of_mass(
+    number_of_spots: npt.NDArray, threshold: int, save: bool = False
+) -> list[tuple[int, int]]:
     """
-    Calculates the center of mass of individual crystals given an 
+    Calculates the center of mass of individual crystals given an
     array containing the numbers of spots with shape (n_rows, n_cols) and plots
     the results.
 
@@ -114,7 +119,7 @@ def plot_muti_crystal_center_of_mass(number_of_spots: npt.NDArray, threshold: in
     array : npt.NDArray
         A numpy array containing the number of spots
     threshold : int
-        We replace the values of the array below the threshold with zeros 
+        We replace the values of the array below the threshold with zeros
     save : bool, optional
         If true, we save the image
     """
@@ -122,27 +127,36 @@ def plot_muti_crystal_center_of_mass(number_of_spots: npt.NDArray, threshold: in
     center_of_mass_list = multi_crystal_center_of_mass(number_of_spots, threshold)
 
     nx, ny = number_of_spots.shape
-    x = np.linspace(0,nx-1,nx)
-    y = np.linspace(0,ny-1,ny)
+    x = np.linspace(0, nx - 1, nx)
+    y = np.linspace(0, ny - 1, ny)
 
-    X,Y = np.meshgrid(x, y, indexing='ij')
+    X, Y = np.meshgrid(x, y, indexing="ij")
 
-    plt.figure() 
-    plt.pcolormesh(Y, X, number_of_spots, edgecolors='w',cmap="viridis")
-    #ax1.set_aspect('equal')
+    plt.figure()
+    plt.pcolormesh(Y, X, number_of_spots, edgecolors="w", cmap="viridis")
     for center_of_mass in center_of_mass_list:
-        print(center_of_mass)
-        plt.scatter(center_of_mass[0], center_of_mass[1], label="CM", marker="+", s=200, color="red")
+        plt.scatter(
+            center_of_mass[0],
+            center_of_mass[1],
+            label="CM",
+            marker="+",
+            s=200,
+            color="red",
+        )
 
     if save:
         plt.savefig("center_of_mass")
-    plt.close()
+    return center_of_mass_list
+
 
 if __name__ == "__main__":
-    number_of_spots = np.array([0 ,1, 0, 0, 120, 0, 0,1,0])
+    number_of_spots = np.array([0, 1, 0, 0, 120, 0, 0, 1, 0])
 
     number_of_spots = number_of_spots.reshape(3, 3)
-    new = np.append(number_of_spots,np.array([0, 0, 0]))
-    rotated_array = np.rot90(np.append(new, number_of_spots).reshape(7,3), k=1)
+    array_with_zeros = np.append(number_of_spots, np.array([0, 0, 0]))
+    rotated_array = np.rot90(
+        np.append(array_with_zeros, number_of_spots).reshape(7, 3), k=1
+    )
     final_test = np.append(rotated_array, rotated_array, axis=0)
+
     plot_muti_crystal_center_of_mass(final_test, threshold=0, save=True)
