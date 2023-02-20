8501e833ee8583964cd12a120ae9a6b344c1ce4b
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-02-20 14:51:11 +1100

    added sim goni

--- mx3_beamline_library/devices/sim/classes/detectors.py
+++ mx3_beamline_library/devices/sim/classes/detectors.py
@@ -1,6 +1,8 @@
 from ophyd import Component as Cpt, Device
-from ophyd.signal import EpicsSignalRO
+from ophyd.signal import EpicsSignalRO,  Signal
 from ophyd.sim import DetWithCountTime
+import numpy.typing as npt
+import numpy as np
 
 
 class BlackFlyCam(Device):
@@ -22,6 +24,44 @@ class BlackFlyCam(Device):
     width = Cpt(EpicsSignalRO, ":image1:ArraySize1_RBV")
     height = Cpt(EpicsSignalRO, ":image1:ArraySize2_RBV")
     array_data = Cpt(EpicsSignalRO, ":image1:ArrayData")
+
+class SimBlackFlyCam(Device):
+    """Ophyd device to acquire images from a simulated Blackfly camera
+
+    Attributes
+    ----------
+    depth: float
+        Depth of the camera image
+    width: float
+        Width of the camera image
+    height: float
+        Height of the camera image
+    array_data : numpy array
+        Array data
+    """
+
+    depth = Cpt(Signal, kind="hinted", value=0)
+    width = Cpt(Signal, kind="hinted", value=0)
+    height = Cpt(Signal, kind="hinted", value=0)
+    data = np.load("/mnt/shares/smd_share/blackfly_cam_images/flat.py")
+    array_data = Cpt(Signal, kind="hinted", value=data)
+    
+
+    def set_values(self, snapshot: npt.NDArray) -> None:
+        """ """
+
+        width: int = 0
+        height: int = 0
+        depth: int = 0
+
+        try:
+            height, width, depth = snapshot.shape
+        except ValueError:
+            height, width = snapshot.shape
+        self.array_data.set(snapshot)
+        self.width.set(width)
+        self.height.set(height)
+        self.depth.set(depth)
 
 
 class MySimDetector(DetWithCountTime):
--- mx3_beamline_library/devices/sim/motors.py
+++ mx3_beamline_library/devices/sim/motors.py
@@ -1,7 +1,9 @@
 """ Simulated motor configuration and instantiation. """
-from .classes.motors import MySimTable, SimulatedPVs
+from .classes.motors import MySimTable, SimulatedPVs, SimMicroDiffractometer
 
 # Simulated testrig motors
 testrig = MySimTable(name="testrig")
+
+md3 = SimMicroDiffractometer(name="sim")
 
 mxcube_sim_PVs = SimulatedPVs(name="mxcube_sim_PVs")
