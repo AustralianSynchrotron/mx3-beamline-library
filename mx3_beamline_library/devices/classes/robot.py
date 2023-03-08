d5317fb05493e5dcbf08f2998013a30d0fb9e89d
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-03-08 15:31:09 +1100

    updated docstrings

--- mx3_beamline_library/devices/classes/robot.py
+++ mx3_beamline_library/devices/classes/robot.py
@@ -4,23 +4,31 @@ from ophyd import Device, Component as Cpt
 from time import sleep
 from mx_robot_library.schemas.common.path import RobotPaths
 from os import environ
+from mx_robot_library.schemas.responses.state import StateResponse 
 
+
+ROBOT_HOST = environ.get("ROBOT_HOST", "12.345.678.9")
+# Create a new client instance
+CLIENT = Client(
+    host=ROBOT_HOST,  # Controller IP
+    status_port=1000,  # Status query port [ENV: MX_ASC_STATUS_PORT"]
+    cmd_port=10000,  # Trajectory command port [ENV: ASC_CMD_PORT"]
+    readonly=False,  # Toggle to block trajectory calls (Local to client)
+)
 
 class State(SignalRO):
     """
-    Ophyd device used to control the phase of the MD3.
-    The accepted phases are Centring, DataCollection, BeamLocation, and
-    Transfer
+    Ophyd device used to read the state of the robot
     """
 
     def __init__(self, name: str, client: Client, *args, **kwargs) -> None:
         """
         Parameters
         ----------
-        motor_name : str
-            Motor Name
-        server : ClientFactory
-            A client Factory object
+        name : str
+            Signal name
+        client : Client
+            A robot client object
 
         Returns
         -------
@@ -31,35 +39,42 @@ class State(SignalRO):
         self.client = client
         self.name = name
 
-    def get(self) -> str:
-        """Gets the current phase
+    def get(self) -> StateResponse:
+        """
+        Gets state of the robot
 
         Returns
         -------
-        str
+        StateResponse
             The current phase
         """
         return self.state
 
     @property
-    def state(self):
+    def state(self) -> StateResponse:
+        """
+        Gets state of the robot
+
+        Returns
+        -------
+        StateResponse
+            The current phase
+        """
         return self.client.status.state
     
 class Mount(Signal):
     """
-    Ophyd device used to control the phase of the MD3.
-    The accepted phases are Centring, DataCollection, BeamLocation, and
-    Transfer
+    Ophyd device used to mount a pin
     """
 
     def __init__(self, name: str, client: Client, *args, **kwargs) -> None:
         """
         Parameters
         ----------
-        motor_name : str
-            Motor Name
-        server : ClientFactory
-            A client Factory object
+        name : str
+            Signal name
+        client : Client
+            A robot client object
 
         Returns
         -------
@@ -71,21 +86,20 @@ class Mount(Signal):
         self.name = name
 
     def get(self) -> str:
-        """Gets the current phase
+        """
+        Checks if there is a pin on the goniometer
 
         Returns
         -------
-        str
-            The current phase
+        StateResponse
+            State response
         """
-        return self.client.status.state.path
+        return self.client.status.state.goni_pin
 
 
-    def _set_and_wait(self, value: dict, timeout: float = None) -> None:
+    def _set_and_wait(self, value: dict, timeout: float = None) -> bytes:
         """
-        Overridable hook for subclasses to override :meth:`.set` functionality.
-        This will be called in a separate thread (`_set_thread`), but will not
-        be called in parallel.
+        Sends the mount command to the robot.
 
         Parameters
         ----------
@@ -96,7 +110,8 @@ class Mount(Signal):
 
         Returns
         -------
-        None
+        bytes
+            The robot response
         """
         
         pin = self.client.utils.get_pin(value["id"], value["puck"])
@@ -113,19 +128,17 @@ class Mount(Signal):
 
 class Unmount(Signal):
     """
-    Ophyd device used to control the phase of the MD3.
-    The accepted phases are Centring, DataCollection, BeamLocation, and
-    Transfer
+    Ophyd device used to unmount a pin
     """
 
     def __init__(self, name: str, client: Client, *args, **kwargs) -> None:
         """
         Parameters
         ----------
-        motor_name : str
-            Motor Name
-        server : ClientFactory
-            A client Factory object
+        name : str
+            Signal name
+        client : Client
+            A robot client object
 
         Returns
         -------
@@ -136,22 +149,21 @@ class Unmount(Signal):
         self.client = client
         self.name = name
 
-    def get(self) -> str:
-        """Gets the current phase
+    def get(self) -> StateResponse:
+        """
+        Checks if there is a pin on the goniometer
 
         Returns
         -------
-        str
-            The current phase
+        StateResponse
+            State response
         """
-        return self.client.status.state.path
+        return self.client.status.state.goni_pin
 
 
-    def _set_and_wait(self, value = None, timeout: float = None) -> None:
+    def _set_and_wait(self, value = None, timeout: float = None) -> bytes:
         """
-        Overridable hook for subclasses to override :meth:`.set` functionality.
-        This will be called in a separate thread (`_set_thread`), but will not
-        be called in parallel.
+        Sends the unmount command to the robot.
 
         Parameters
         ----------
@@ -162,7 +174,8 @@ class Unmount(Signal):
 
         Returns
         -------
-        None
+        bytes
+            The robot response
         """
         
         # Try to unmount a pin
@@ -178,62 +191,7 @@ class Unmount(Signal):
         return msg
 
 
-ROBOT_HOST = environ.get("ROBOT_HOST", "12.345.678.9")
-# Create a new client instance
-CLIENT = Client(
-    host=ROBOT_HOST,  # Controller IP
-    status_port=1000,  # Status query port [ENV: MX_ASC_STATUS_PORT"]
-    cmd_port=10000,  # Trajectory command port [ENV: ASC_CMD_PORT"]
-    readonly=False,  # Toggle to block trajectory calls (Local to client)
-)
-
-
 class IsaraRobot(Device):
     state = Cpt(State, name="state", client=CLIENT)
     mount = Cpt(Mount, name="mount", client=CLIENT)
     unmount = Cpt(Unmount, name="unmount", client=CLIENT)
-
-
-if __name__ == "__main__":
-    from bluesky import RunEngine
-    from bluesky.callbacks.best_effort import BestEffortCallback
-    from bluesky.plan_stubs import mv
-
-    
-    isara_robot = IsaraRobot(name="robot")
-
-    print(isara_robot.state.get())
-
-    RE = RunEngine()
-    bec = BestEffortCallback()
-    RE.subscribe(bec)
-
-    def mount_sample(mount_signal: Mount, id: int, puck: int):
-        """
-        Mounts a sample given an id and puck
-
-        Parameters
-        ----------
-        mount_signal : Mount
-            A robot mount signal
-        id : int
-            id
-        puck : int
-            Puck
-
-        Yields
-        ------
-        _type_
-            _description_
-        """
-        yield from mv(mount_signal, {"id": id ,"puck": puck})
-
-    def unmount_sample(unmount_signal: Unmount):
-        yield from mv(unmount_signal, None)
-
-    mount_signal = isara_robot.mount
-    unmount_signal = isara_robot.unmount
-
-    #RE(mount_sample(mount_signal, id=1, puck=1))
-    RE(unmount_sample(unmount_signal))
-
--- mx3_beamline_library/examples/robot.py
+++ mx3_beamline_library/examples/robot.py
@@ -3,15 +3,12 @@ This example shows how to mount and unmount samples using the bluesky run engine
 """
 from os import environ
 environ["ROBOT_HOST"] = "12.345.678.9" # noqa
+environ["BL_ACTIVE"] = "True"
 
 from bluesky import RunEngine
 from bluesky.callbacks.best_effort import BestEffortCallback
 from mx3_beamline_library.devices.motors import isara_robot
 from mx3_beamline_library.plans.robot import mount_pin, unmount_pin
-
-
-
-print(isara_robot.state.get())
 
 RE = RunEngine()
 bec = BestEffortCallback()
