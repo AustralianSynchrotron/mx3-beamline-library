01a1b80453222c74bf3e9078ebde69bb0a66a1a9
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-03-22 13:40:53 +1100

    added x ray centering example

---
+++ mx3_beamline_library/examples/x_ray_centering.py
@@ -0,0 +1,51 @@
+"""
+This example runs an optical and xray centering plan. To run this example,
+you should have a running instance of the sim-plon API. For details on how to run the
+sim-plon api, refer to https://bitbucket.synchrotron.org.au/scm/mx3/mx-sim-plon-api.git,
+and make sure that the DECTRIS_DETECTOR_HOST and DECTRIS_DETECTOR_PORT (see below) are
+configured accordingly.
+"""
+import time
+from os import environ
+
+from bluesky import RunEngine
+from bluesky.callbacks.best_effort import BestEffortCallback
+
+environ["DECTRIS_DETECTOR_HOST"] = "10.244.101.200"
+environ["DECTRIS_DETECTOR_PORT"] = "80"
+environ["BL_ACTIVE"] = "True"
+environ["MD_REDIS_HOST"] = "10.244.101.30"
+environ["MD_REDIS_PORT"] = "6379"
+from mx3_beamline_library.devices import detectors, motors  # noqa
+from mx3_beamline_library.devices.detectors import md_camera  # noqa
+from mx3_beamline_library.devices.motors import md3  # noqa
+from mx3_beamline_library.plans.xray_centering import (  # noqa
+    XRayCentering,
+)
+
+# Configure the detector to send one frame per trigger
+# REST = "http://0.0.0.0:8000"
+# nimages = {"value": 1}
+# r = requests.put(f"{REST}/detector/api/1.8.0/config/nimages", json=nimages)
+
+dectris_detector = detectors.dectris_detector
+
+
+# Instantiate run engine an start plan
+RE = RunEngine({})
+bec = BestEffortCallback()
+RE.subscribe(bec)
+print(md3.phase.get())
+
+t = time.perf_counter()
+xray_centering = XRayCentering(
+    sample_id="my_sample",
+    detector=dectris_detector,
+    omega=md3.omega,
+    zoom=md3.zoom,
+    grid_scan_type="flat",
+    
+)
+RE(xray_centering.start_grid_scan())
+
+print(f"Execution time: {time.perf_counter() - t}")
