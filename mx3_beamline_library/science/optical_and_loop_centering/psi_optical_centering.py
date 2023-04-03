bdfd030062d1b2d2eca41ebfb61417d7c51dfaa9
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-04-03 13:17:18 +1000

    added top camera to optical centering plan

--- mx3_beamline_library/science/optical_and_loop_centering/psi_optical_centering.py
+++ mx3_beamline_library/science/optical_and_loop_centering/psi_optical_centering.py
@@ -52,7 +52,7 @@ class loopImageProcessing(object):
                 "dilate": True,
                 "dilateIter": 1,
                 "dilateKernel": np.ones((1, 1), np.uint8),
-            }
+            },
         }
 
         paramsX06DA = {
