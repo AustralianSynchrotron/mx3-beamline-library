372769eb9bce587d3e5c9a10b3a2f0414e731732
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-04-19 12:55:40 +1000

    updated constats

--- mx3_beamline_library/constants.py
+++ mx3_beamline_library/constants.py
@@ -4,10 +4,8 @@ import numpy as np
 import numpy.typing as npt
 
 """ Library scoped constants. """
-PV_PREFIX = "SR##ID"
 
-TOP_CAMERA_BACKGROUND_IMAGE = path_to_config_file = path.join(
+TOP_CAMERA_BACKGROUND_IMAGE = path.join(
     path.dirname(__file__), "plans/configuration/top_camera_background_image.npy"
 )
-
 top_camera_background_img_array: npt.NDArray = np.load(TOP_CAMERA_BACKGROUND_IMAGE)
