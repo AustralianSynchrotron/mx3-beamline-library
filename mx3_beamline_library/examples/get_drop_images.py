adf6cb870c7f4140ab4d3fe76bb5ef9a11d7bd7f
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-05-29 12:23:32 +1000

    added image analysis file to reuse plans

--- mx3_beamline_library/examples/get_drop_images.py
+++ mx3_beamline_library/examples/get_drop_images.py
@@ -1,21 +1,30 @@
-
+"""
+This examples shows how to take snapshots of drops at positions specified
+in the drop_location list
+"""
 from os import environ
 
 from bluesky import RunEngine
 from bluesky.callbacks.best_effort import BestEffortCallback
 
-environ["BL_ACTIVE"] = "True"
-environ["MD3_ADDRESS"] = "10.244.101.30"
+environ["BL_ACTIVE"] = "False"
+environ["MD3_ADDRESS"] = "12.345.678.90"
 environ["MD3_PORT"] = "9001"
-environ["MD_REDIS_HOST"] = "10.244.101.30"
+environ["MD_REDIS_HOST"] = "12.345.678.90"
 environ["MD_REDIS_PORT"] = "6379"
 
-from mx3_beamline_library.plans.tray_scans import get_drop_images
+from mx3_beamline_library.plans.tray_scans import save_drop_snapshots  # noqa
 
 # Instantiate run engine an start plan
 RE = RunEngine({})
 bec = BestEffortCallback()
 RE.subscribe(bec)
 
-drop_locations = ["B1-1"]
-RE(get_drop_images(drop_locations, alignment_y_offset=0.25, alignment_z_offset=-1.0,))
+RE(
+    save_drop_snapshots(
+        tray_id="my_tray",
+        drop_locations=["B1-1", "C1-1"],
+        alignment_y_offset=0.25,
+        alignment_z_offset=-1.0,
+    )
+)
--- mx3_beamline_library/plans/tray_scans.py
+++ mx3_beamline_library/plans/tray_scans.py
@@ -1,8 +1,11 @@
 import logging
-from os import environ
+from os import environ, getcwd, mkdir, path
+from time import sleep
 from typing import Generator, Optional
 
-from bluesky.plan_stubs import configure, mv, stage, trigger_and_read, unstage  # noqa
+import matplotlib.pyplot as plt
+import numpy as np
+from bluesky.plan_stubs import mv
 from bluesky.utils import Msg
 
 from ..devices.classes.detectors import DectrisDetector
@@ -10,17 +13,7 @@ from ..devices.motors import md3
 from ..schemas.detector import UserData
 from ..schemas.xray_centering import MD3ScanResponse
 from .basic_scans import arm_trigger_and_disarm_detector, md3_grid_scan
-
-
-from mx3_beamline_library.schemas.detector import UserData
-from bluesky.plan_stubs import mv
-import numpy as np
-import cv2
-from mx3_beamline_library.devices.detectors import md_camera  # noqa
-from time import sleep
-import matplotlib.pyplot as plt
-from mx3_beamline_library.devices.motors import md3
-
+from .image_analysis import get_image_from_md3_camera, unblur_image
 
 logger = logging.getLogger(__name__)
 _stream_handler = logging.StreamHandler()
@@ -252,172 +245,60 @@ def multiple_drop_grid_scan(
         )
 
 
-
-
-
-
-
-def unblur_image(
-    focus_motor,
-    a: float = 0.0,
-    b: float = 1.0,
-    tol: float = 0.2,
+def save_drop_snapshots(
+    tray_id: str,
+    drop_locations: list[str],
+    alignment_y_offset: float = 0.25,
+    alignment_z_offset: float = -1.0,
+    min_focus: float = -1.0,
+    max_focus: float = 0.5,
+    tol: float = 0.1,
     number_of_intervals: int = 2,
-):
+    output_directory: Optional[str] = None,
+) -> Generator[Msg, None, None]:
     """
-    We use the Golden-section search to find the global maximum of the variance function
-    described in the calculate_variance method ( `var( Img * L(x,y) )` )
-    (see the definition of self.variance_local_maximum).
-    In order to find the global maximum, we search for local maximums in N number of
-    sub-intervals defined by number_of_intervals.
+    This plan takes drop snapshots at the positions specified by the
+    drop_location list. The results are saved to the output_directory.
+    Whenever we move the md3 to a new drop location, we unblur the image.
 
     Parameters
     ----------
-    motor : MD3Motor
-        An MD3 motor. We can focus the image with either alignment x, or sample_x and
-        sample_y (depending on the value of omega)
-    a : float, optional
-        Minimum value to search for the maximum of var( Img * L(x,y) )
-    b : float, optional
-        Maximum value to search for the maximum of var( Img * L(x,y) )
+    tray_id : str
+        The id of the tray
+    drop_locations : list[str]
+        A list of drop locations, e.g ["A1-1", "B2-2"]
+    alignment_y_offset : float, optional
+        Alignment y offset, by default 0.25
+    alignment_z_offset : float, optional
+        Alignment z offset, by default -1.0
+    min_focus : float, optional
+        Minimum value to search for the maximum of var( Img * L(x,y) ),
+        by default -1.0
+    max_focus : float, optional
+        Maximum value to search for the maximum of var( Img * L(x,y) ),
+        by default 0.5
     tol : float, optional
-        The tolerance, by default 0.2
+        The tolerance used by the Golden-section search, by default 0.1
     number_of_intervals : int, optional
-        Number of sub-intervals used to find the global maximum of a multimodal function
+        Number of intervals used to find local maximums of the function
+        `var( Img * L(x,y) )`, by default 2
+    output_directory: Optional[str] = None,
+        The output directory. If output_directory=None, the results are
+        saved to the current working directory
 
     Yields
     ------
     Generator[Msg, None, None]
-        Moves the focus motor to a position where the image is focused
+        _description_
     """
-
-    # Create sub-intervals to find the global maximum
-    step = (b - a) / number_of_intervals
-    interval_list = []
-    for i in range(number_of_intervals):
-        interval_list.append((a + step * i, a + step * (i + 1)))
-
-    # Calculate local maximums
-    laplacian_list = []
-    focus_motor_pos_list = []
-    for interval in interval_list:
-        yield from variance_local_maximum(
-            focus_motor, interval[0], interval[1], tol
-        )
-        laplacian_list.append(calculate_variance())
-        focus_motor_pos_list.append(focus_motor.position)
-
-    # Find global maximum, and move the focus motor to the best focused position
-    argmax = np.argmax(np.array(laplacian_list))
-    yield from mv(focus_motor, focus_motor_pos_list[argmax])
-
-def variance_local_maximum(
-    focus_motor,
-    a: float = 0.0,
-    b: float = 1.0,
-    tol: float = 0.2,
-) -> float:
-    """
-    We use the Golden-section search to find the local maximum of the variance function
-    described in the calculate_variance method ( `var( Img * L(x,y) )` ).
-    NOTE: We assume that the function is strictly unimodal on [a,b].
-    See for example: https://en.wikipedia.org/wiki/Golden-section_search
-
-    Parameters
-    ----------
-    focus_motor : MD3Motor
-        An MD3 motor, can be either a combination of sample_x and
-        sample_y, or alignment_x
-    a : float
-        Minimum value to search for the maximum of var( Img * L(x,y) )
-    b : float
-        Maximum value to search for the maximum of var( Img * L(x,y) )
-    tol : float, optional
-        The tolerance, by default 0.2
-
-    Returns
-    -------
-    Generator[Msg, None, None]
-        Moves sample_y to a position where the image is focused
-    """
-    gr = (np.sqrt(5) + 1) / 2
-
-    c = b - (b - a) / gr
-    d = a + (b - a) / gr
-
-    count = 0
-    while abs(b - a) > tol:
-        yield from mv(focus_motor, c)
-        val_c = calculate_variance()
-
-        yield from mv(focus_motor, d)
-        val_d = calculate_variance()
-
-        if val_c > val_d:  # val_c > val_d to find the maximum
-            b = d
-        else:
-            a = c
-
-        # We recompute both c and d here to avoid loss of precision which
-        # may lead to incorrect results or infinite loop
-        c = b - (b - a) / gr
-        d = a + (b - a) / gr
-        count += 1
-
-    maximum = (b + a) / 2
-    yield from mv(focus_motor, maximum)
-
-def calculate_variance() -> float:
-    """
-    We calculate the variance of the convolution of the laplacian kernel with an image,
-    e.g. var( Img * L(x,y) ), where Img is an image taken from the camera ophyd object,
-    and L(x,y) is the Laplacian kernel.
-
-    Returns
-    -------
-    float
-        var( Img * L(x,y) )
-    """
-    data = get_image_from_md3_camera(np.uint8)
+    if output_directory is None:
+        output_directory = getcwd()
 
     try:
-        gray_image = cv2.cvtColor(data, cv2.COLOR_RGB2GRAY)
-    except cv2.error:
-        # The MD3 camera already returns black and white images for the zoom levels
-        # 5, 6 and 7, so we don't do anything here
-        gray_image = data
+        mkdir(path.join(output_directory, tray_id))
+    except FileExistsError:
+        logger.info("Folder exists. Overwriting results")
 
-    return cv2.Laplacian(gray_image, cv2.CV_64F).var()
-
-def get_image_from_md3_camera( dtype):
-    """
-    Gets a frame from the md3.
-
-    Parameters
-    ----------
-    dtype : npt.DTypeLike, optional
-        The data type of the numpy array, by default np.uint16
-
-    Returns
-    -------
-    npt.NDArray
-        A frame of shape (height, width, depth)
-    """
-    if environ["BL_ACTIVE"].lower() == "true":
-        array_data = md_camera.array_data.get()
-        data = array_data.astype(dtype)
-    else:
-        # When the camera is not working, we stream a static image
-        # of the test rig
-        data = (
-            np.load("/mnt/shares/smd_share/blackfly_cam_images/flat.npy")
-            .astype(dtype)
-            .astype(dtype)
-        )
-    return data
-
-
-def get_drop_images(drop_locations: list[str], alignment_y_offset, alignment_z_offset):
     drop_locations.sort()  # sort list to scan drops faster
     for drop in drop_locations:
 
@@ -431,18 +312,20 @@ def get_drop_images(drop_locations: list[str], alignment_y_offset, alignment_z_o
             drop[2] == "-"
         ), "The drop location should follow a format similar to e.g. A1-1"
         _drop = int(drop[3]) - 1  # We count from 0, not 1
-        yield from mv(md3.move_plate_to_shelf, (row, column, _drop) )
+        yield from mv(md3.move_plate_to_shelf, (row, column, _drop))
 
         sleep(1)
         start_alignment_y = md3.alignment_y.position + alignment_y_offset
         start_alignment_z = md3.alignment_z.position + alignment_z_offset
 
         yield from mv(md3.alignment_y, start_alignment_y)
-        yield from mv( md3.alignment_z, start_alignment_z)
+        yield from mv(md3.alignment_z, start_alignment_z)
         sleep(1)
-        yield from unblur_image(md3.alignment_x, -1, 0.5, 0.1, number_of_intervals=2)
-        
+        yield from unblur_image(
+            md3.alignment_x, min_focus, max_focus, tol, number_of_intervals
+        )
+
         plt.figure()
         plt.imshow(get_image_from_md3_camera(np.uint16))
-        plt.savefig(drop)
+        plt.savefig(path.join(output_directory, tray_id, drop))
         plt.close()
