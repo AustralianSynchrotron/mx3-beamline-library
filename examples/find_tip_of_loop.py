bbba3c6ba943eb004b6623b574e176e9103ec95e
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-08-16 16:27:40 +1000

    added example to find the tip of a loop

---
+++ examples/find_tip_of_loop.py
@@ -0,0 +1,20 @@
+"""
+This examples shows how to find the tip of a loop, and determines 
+where its grid coordinates lie for raster scanning
+"""
+
+from mx3_beamline_library.science.optical_and_loop_centering.loop_edge_detection import LoopEdgeDetection
+from mx3_beamline_library.devices.sim.classes.detectors import SIM_MD3_CAMERA_IMG
+
+loop_edge_detections = LoopEdgeDetection(image=SIM_MD3_CAMERA_IMG, block_size=35, adaptive_constant=3)
+
+# Find tp of the loop
+tip = loop_edge_detections.find_tip()
+print("tip of the loop", tip)
+
+# Find the raster grid coordinates
+rectangle_coordinates = loop_edge_detections.fit_rectangle()
+print("Rectangle coordinates", rectangle_coordinates)
+
+# Plot raster grid
+loop_edge_detections.plot_raster_grid(rectangle_coordinates, filename="result")
