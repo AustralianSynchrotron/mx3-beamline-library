9751bf8f5ab49dd4c2be798e6f731039b316a568
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-08-23 13:55:17 +1000

    removed sim registry

--- mx3_beamline_library/devices/sim/classes/detectors.py
+++ mx3_beamline_library/devices/sim/classes/detectors.py
@@ -14,7 +14,6 @@ path_to_sim_images = path.join(path.dirname(__file__), "../sim_images")
 SIM_TOP_CAMERA_IMG = np.load(path.join(path_to_sim_images, "top_camera.npy"))
 SIM_MD3_CAMERA_IMG = np.load(path.join(path_to_sim_images, "md3_image.npy"))
 
-@Register("Grasshopper Camera")
 class GrasshopperCamera(SimDetectorCam):
     pass
 
