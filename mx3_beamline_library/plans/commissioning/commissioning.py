dfd0a33540cad43720ca0be2f2e6ed83863fb2dc
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-08-28 16:01:22 +1000

    updated mx3 1d scan

--- mx3_beamline_library/plans/commissioning/commissioning.py
+++ mx3_beamline_library/plans/commissioning/commissioning.py
@@ -1,23 +1,71 @@
-from ...devices.classes.detectors import GrasshopperCamera, HDF5Filewriter
-import uuid
+from datetime import datetime, timezone
+from typing import Callable, Generator, Union
+
+from bluesky.plan_stubs import mv
 from bluesky.plans import scan
-from bluesky.plan_stubs import mv, read
+from bluesky.utils import Msg
+from ophyd.areadetector.base import EpicsSignalWithRBV
+from ophyd.epics_motor import EpicsMotor
+
+from ...devices.classes.detectors import GrasshopperCamera, HDF5Filewriter
+
 
 def mx3_1d_scan(
-    detectors: list[GrasshopperCamera], 
-    motor,
-    initial_position,
-    final_position,
-    number_of_steps,
-    num=None, 
-    per_step=None, 
-    metadata: dict=None, 
-    ):
+    detectors: list[Union[GrasshopperCamera, HDF5Filewriter, EpicsSignalWithRBV]],
+    motor: EpicsMotor,
+    initial_position: float,
+    final_position: float,
+    number_of_steps: int,
+    num: int = None,
+    per_step: Callable = None,
+    metadata: dict = None,
+    hdf5_filename: str = None,
+) -> Generator[Msg, None, None]:
+    """
+    Wrapper of the bluesky scan function
 
-    for detector in detectors:
-        detector.kind = "hinted"
+    Parameters
+    ----------
+    detectors : list[Union[GrasshopperCamera, HDF5Filewriter, EpicsSignalWithRBV]]
+        A list of detectors. Add here all parameters related to statistics that have to
+        be calculated during the scan, e.g. [my_camera.stats.total, my_camera.stats.sigma]
+    motor : EpicsMotor
+        The motor used in the plan
+    initial_position : float
+        The motor initial position
+    final_position : float
+        the motor final position
+    number_of_steps : int
+        The number of steps of the scan
+    num : int, optional
+        Number of points, by default None
+    per_step : Callable, optional
+        Hook for customizing action of inner loop (messages per step).
+        See docstring of bluesky.plan_stubs.one_nd_step() (the default)
+        for details., by default None
+    metadata : dict, optional
+        Metadata, by default None
+    hdf5_filename : str, optional
+        The name of the HDF5 file generated during the run. If not provided,
+        the file will be named based on the date and (UTC) time of generation,
+        for example
+        mx3_1d_scan_28-08-2023_05:44:15.h5
 
-    hdf5_filename = str(uuid.uuid4()) + ".h5"
+    Yields
+    ------
+    Generator[Msg, None, None]
+        A bluesky plan
+
+    Raises
+    ------
+    ValueError
+        An error if metadata is not a dictionary
+    """
+    if hdf5_filename is None:
+        # datetime object containing current date and time
+        now = datetime.now(tz=timezone.utc)
+        dt_string = now.strftime("%d-%m-%Y_%H:%M:%S")
+        hdf5_filename = "mx3_1d_scan_" + dt_string + ".h5"
 
     if metadata is None:
         metadata = {"hdf5_filename": hdf5_filename}
@@ -25,16 +73,27 @@ def mx3_1d_scan(
         metadata.update({"hdf5_filename": hdf5_filename})
     elif type(metadata) is not dict:
         raise ValueError("Metadata must be a dictionary")
-    
+
     for detector in detectors:
+        detector.kind = "hinted"
+
         if type(detector) == HDF5Filewriter:
-            number_of_frames = number_of_steps
+            detector: HDF5Filewriter
             yield from mv(
-                detector.filename, 
-                hdf5_filename, 
+                detector.filename,
+                hdf5_filename,
                 detector.frames_per_datafile,
-                number_of_frames
+                number_of_steps,
             )
             write_path_template = detector.write_path_template.get()
             metadata.update({"write_path_template": write_path_template})
-    yield from scan(detectors, motor, initial_position, final_position, number_of_steps,num=num, per_step=per_step, md=metadata)
+    yield from scan(
+        detectors,
+        motor,
+        initial_position,
+        final_position,
+        number_of_steps,
+        num=num,
+        per_step=per_step,
+        md=metadata,
+    )
--- mx3_beamline_library/plans/commissioning/utils.py
+++ mx3_beamline_library/plans/commissioning/utils.py
@@ -1,22 +1,56 @@
-import tiled
 from os import path
+
+import tiled
 from tiled.queries import Contains
 
 
-def get_hdf5_file_path_from_bluesky_scan_id(tiled_client: tiled.client.constructors.from_uri, bluesky_scan_id: str):
+def get_hdf5_file_path_from_bluesky_scan_id(
+    tiled_client: tiled.client.constructors.from_uri, bluesky_scan_id: str
+) -> str:
+    """
+    Gets the hdf5 file path from the bluesky scan id
+
+    Parameters
+    ----------
+    tiled_client : tiled.client.constructors.from_uri
+        A tiled client
+    bluesky_scan_id : str
+        The bluesky unique scan id
+
+    Returns
+    -------
+    str
+       The path of the hdf5 file generated during the run
+    """
     start_document = tiled_client[bluesky_scan_id].metadata["start"]
-    hdf5_file_path = path.join(start_document["write_path_template"], start_document["hdf5_filename"])
+    hdf5_file_path = path.join(
+        start_document["write_path_template"], start_document["hdf5_filename"]
+    )
     return hdf5_file_path
 
+
 def get_bluesky_scan_id_from_hdf5_filename(
-        tiled_client: tiled.client.constructors.from_uri, 
-        hdf5_filename: str): #returns bluesky run
-    # hdf5_filename can be either the full path or just the filename
-    
+    tiled_client: tiled.client.constructors.from_uri, hdf5_filename: str
+):
+    """
+    Gets the bluesky documents from the name of a hdf5 file.
+    The name of the hdf5 file can include either the pull path, or the
+    name of the file, e.g., /path/to/file/file.h5 or file.h5
+
+    Parameters
+    ----------
+    tiled_client : tiled.client.constructors.from_uri
+        A tiled client
+    hdf5_filename : str
+        The name of the hdf5 file. Can include the full path,
+        or just the name of the file.
+
+    Returns
+    -------
+        The bluesky documents associated with a hdf5 file
+    """
+
     value = path.basename(path.normpath(hdf5_filename))
-    bluesky_run = tiled_client.search(Contains(
-        key="hdf5_filename", value=value))[0]
+    bluesky_run = tiled_client.search(Contains(key="hdf5_filename", value=value))[0]
 
     return bluesky_run
-
-
