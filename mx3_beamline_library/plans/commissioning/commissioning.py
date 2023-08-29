17a818684a831ff4e96ef256a326f0e00dac4d95
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-08-29 16:39:47 +1000

    stop plan 1d scan plan based on a given criteria

--- mx3_beamline_library/plans/commissioning/commissioning.py
+++ mx3_beamline_library/plans/commissioning/commissioning.py
@@ -1,12 +1,14 @@
 from datetime import datetime, timezone
 from typing import Callable, Generator, Union
 
-from bluesky.plan_stubs import mv
+import logging
+from bluesky.plan_stubs import mv, move_per_step, trigger_and_read, read
 from bluesky.plans import scan
 from bluesky.utils import Msg
 from ophyd.areadetector.base import EpicsSignalWithRBV
 from ophyd.epics_motor import EpicsMotor
-
+from ophyd import Signal
+import numpy as np
 from ...devices.classes.detectors import GrasshopperCamera, HDF5Filewriter
 
 
@@ -17,7 +19,7 @@ def mx3_1d_scan(
     final_position: float,
     number_of_steps: int,
     num: int = None,
-    per_step: Callable = None,
+    #per_step: Callable = None,
     metadata: dict = None,
     hdf5_filename: str = None,
 ) -> Generator[Msg, None, None]:
@@ -62,7 +64,6 @@ def mx3_1d_scan(
         An error if metadata is not a dictionary
     """
     if hdf5_filename is None:
-        # datetime object containing current date and time
         now = datetime.now(tz=timezone.utc)
         dt_string = now.strftime("%d-%m-%Y_%H:%M:%S")
         hdf5_filename = "mx3_1d_scan_" + dt_string + ".h5"
@@ -87,6 +88,14 @@ def mx3_1d_scan(
             )
             write_path_template = detector.write_path_template.get()
             metadata.update({"write_path_template": write_path_template})
+
+
+    _stats_buffer = Signal(
+        name="buffer", kind="omitted", value=[]
+    )
+    _stop_plan_signal = Signal(name="stop_plan", kind="omitted", value=False)
+    detectors.append(_stop_plan_signal)
+    detectors.append(_stats_buffer, )
     yield from scan(
         detectors,
         motor,
@@ -94,6 +103,73 @@ def mx3_1d_scan(
         final_position,
         number_of_steps,
         num=num,
-        per_step=per_step,
+        per_step=_one_nd_step,
         md=metadata,
     )
+
+def _one_nd_step(detectors, step, pos_cache, take_reading=trigger_and_read):
+    """
+    Inner loop of an N-dimensional step scan
+
+    This is the default function for ``per_step`` param`` in ND plans.
+
+    Parameters
+    ----------
+    detectors : iterable
+        devices to read
+    step : dict
+        mapping motors to positions in this step
+    pos_cache : dict
+        mapping motors to their last-set positions
+    take_reading : plan, optional
+        function to do the actual acquisition ::
+
+           def take_reading(dets, name='primary'):
+                yield from ...
+
+        Callable[List[OphydObj], Optional[str]] -> Generator[Msg], optional
+
+        Defaults to `trigger_and_read`
+    """
+    _stop_plan_signal: Signal = detectors[-2]
+    if not _stop_plan_signal.get():
+        motors = step.keys()
+        yield from move_per_step(step, pos_cache)
+        reading = yield from take_reading(list(detectors) + list(motors))
+
+        _stats_buffer = detectors[-1]
+        for key in reading.keys():
+            # TODO: for now we focus on stats.total, but in principle this parameter
+            # can be anything we want
+            key_index = key.find("total")
+            if key_index >=0:
+                value = reading[key]["value"]
+                stats_list =  yield from read(_stats_buffer)
+                stats_list["buffer"]["value"].append(value)
+                yield from mv(_stats_buffer, stats_list["buffer"]["value"])
+
+        stop_plan = _stop_plan(stats_list["buffer"]["value"])
+        yield from mv(_stop_plan_signal, stop_plan)
+
+def _stop_plan(stats_list: list) -> bool:
+    if len(stats_list) < 2:
+        return False
+    else:
+        second_derivative = np.gradient(np.gradient(stats_list))
+        sign_diff = np.diff(np.sign(second_derivative))
+        inflection_points = np.where(sign_diff)[0]
+        if len(inflection_points) == 0:
+            return False
+        else:
+            maximum = np.where(second_derivative[inflection_points] <0)[0]
+            if len(maximum)>0:
+                difference = abs(np.max(stats_list) - stats_list[-1])
+                # TODO: we now tolerate 3 sigma difference, but I just made up this number
+                if difference > np.std(stats_list):
+                    return True
+                else:
+                    return False
+            else:
+
+                return False
+
