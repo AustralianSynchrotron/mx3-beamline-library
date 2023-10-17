af0c480fb4e6f165fed3fad7f4602a824d42352b
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-10-17 12:32:38 +1100

    pre-commit

--- examples/screening.py
+++ examples/screening.py
@@ -8,14 +8,14 @@ This example runs a screening plan
         simulated motors as used as a replacement.
 """
 import time
+from os import environ
 
 from bluesky import RunEngine
 from bluesky.callbacks.best_effort import BestEffortCallback
-from os import environ
 
 # Modify the following ENV variables with the corresponding
 # hosts and ports.
-#IF BL_ACTIVE=False, we run the library in simulation mode
+# IF BL_ACTIVE=False, we run the library in simulation mode
 environ["BL_ACTIVE"] = "False"
 environ["DECTRIS_DETECTOR_HOST"] = "12.345.678.90"
 environ["DECTRIS_DETECTOR_PORT"] = "80"
@@ -24,8 +24,8 @@ environ["MD_REDIS_PORT"] = "6379"
 environ["MD3_ADDRESS"] = "12.345.678.90"
 environ["MD3_PORT"] = "9001"
 from mx3_beamline_library.devices import detectors  # noqa
-from mx3_beamline_library.plans.basic_scans import md3_scan
-from mx3_beamline_library.schemas.crystal_finder import MotorCoordinates
+from mx3_beamline_library.plans.basic_scans import md3_scan  # noqa
+from mx3_beamline_library.schemas.crystal_finder import MotorCoordinates  # noqa
 
 dectris_detector = detectors.dectris_detector
 
@@ -39,17 +39,16 @@ t = time.perf_counter()
 screening = md3_scan(
     id="my_sample",
     motor_positions=MotorCoordinates(
-        sample_x=0, 
+        sample_x=0,
         sample_y=0,
         alignment_x=0.434,
         alignment_y=0,
         alignment_z=0,
         omega=0,
-        ),
+    ),
     number_of_frames=10,
     scan_range=10,
     exposure_time=1,
-
 )
 RE(screening)
 
