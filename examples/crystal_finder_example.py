dce881729bde23f52b1d17df35d242fe6e7adb82
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2024-02-05 11:29:37 +1100

    updated crystal finder example

--- examples/crystal_finder_example.py
+++ examples/crystal_finder_example.py
@@ -19,25 +19,15 @@ number_of_spots = 100 * np.exp(-((d - mu) ** 2 / (2.0 * sigma**2)))
 
 # Run the crystal finder for the first grid scan
 crystal_finder = CrystalFinder(number_of_spots=number_of_spots, threshold=5)
-(
-    coords_flat,
-    distance_flat,
-    max_number_of_spots_flat,
-) = crystal_finder.plot_crystal_finder_results(save=True, filename="flat")
+crystals_flat = crystal_finder.plot_crystal_finder_results(save=True, filename="flat")
 
 # Run the crystal finder for the second grid scan
 crystal_finder = CrystalFinder(number_of_spots=number_of_spots, threshold=5)
-(
-    coords_edge,
-    distance_edge,
-    max_number_of_spots_edge,
-) = crystal_finder.plot_crystal_finder_results(save=True, filename="edge")
+crystals_edge = crystal_finder.plot_crystal_finder_results(save=True, filename="edge")
 
 # Find the crystals volumes
 crystal_finder_3d = CrystalFinder3D(
-    crystal_coordinates_flat=coords_flat,
-    crystal_coordinates_edge=coords_edge,
-    dist_flat=distance_flat,
-    dist_edge=distance_edge,
+    crystals_flat=crystals_flat,
+    crystals_edge=crystals_edge,
 )
 crystal_volumes = crystal_finder_3d.plot_crystals(save=True, filename="3d_results")
