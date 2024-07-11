a4e1f028f84b3c19da7b3959dec7e9f91ed95bc9
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2024-07-11 11:14:48 +1000

    add ion pump and ccg ophyd devices

---
+++ mx3_beamline_library/devices/classes/vacuum.py
@@ -0,0 +1,59 @@
+from ophyd import Device, Component as Cpt, EpicsSignalRO, Kind
+from . import Register
+
+from ophyd import Device, Kind, EpicsSignalRO, Component as Cpt
+
+@Register("Cold Cathode Gauge")
+class ColdCathodeGaugeController(Device):
+    pressure_monitor = Cpt(
+        EpicsSignalRO,
+        ":PRESSURE_MONITOR",
+        kind=Kind.omitted,
+        auto_monitor=True,
+        doc="IP calc pressure",
+    )
+    status = Cpt(
+        EpicsSignalRO,
+        ":STATUS",
+        kind=Kind.omitted,
+        auto_monitor=True,
+        doc="IP run status",
+    )
+
+@Register("Ion Pump")
+class IonPumpController(Device):
+    pressure_monitor = Cpt(
+        EpicsSignalRO,
+        ":PRESSURE_MONITOR",
+        kind=Kind.omitted,
+        auto_monitor=True,
+        doc="IP calc pressure",
+    )
+    status = Cpt(
+        EpicsSignalRO,
+        ":STATUS",
+        kind=Kind.omitted,
+        auto_monitor=True,
+        doc="IP run status",
+    )
+    current_monitor = Cpt(
+        EpicsSignalRO,
+        ":CURRENT_MONITOR",
+        kind=Kind.omitted,
+        auto_monitor=True,
+        doc="IP current readback (uA)",
+    )
+    current_monitor_egu= Cpt(
+        EpicsSignalRO,
+        ":CURRENT_MONITOR.EGU",
+        kind=Kind.omitted,
+        auto_monitor=True,
+        doc="IP current units readback",
+    )
+    voltage_monitor = Cpt(
+        EpicsSignalRO,
+        ":VOLTAGE_MONITOR",
+        kind=Kind.omitted,
+        auto_monitor=True,
+        doc="IP HV readback (kV)",
+    )
