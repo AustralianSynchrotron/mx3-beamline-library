a13da81da20d1395fd941a76ac2a336844114468
Francisco Hernandez Vivanco <49014169+fhernandezvivanco@users.noreply.github.com>
2024-09-10 11:48:59 +1000

    adds energy and attenuation PVs (#42)

--- mx3_beamline_library/devices/sim/optics.py
+++ mx3_beamline_library/devices/sim/optics.py
@@ -1,3 +1,7 @@
 from ophyd import Signal
 
-exp_shutter = Signal(name="Experimental Shutter")
+energy_master = Signal(name="energy_master")
+
+energy_dmm = Signal(name="energy_dmm")
+
+attenuation = Signal(name="attenuation")
