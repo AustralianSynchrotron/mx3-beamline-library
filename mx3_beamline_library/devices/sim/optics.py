2d741ab98402c2ecc1d43ece42fe7810c902d360
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2024-12-05 16:15:03 +1100

    update default value

--- mx3_beamline_library/devices/sim/optics.py
+++ mx3_beamline_library/devices/sim/optics.py
@@ -4,6 +4,6 @@ energy_master = Signal(name="energy_master", value=13.0)
 
 energy_dmm = Signal(name="energy_dmm", value=13.0)
 
-attenuation = Signal(name="attenuation", value=10.0)
+attenuation = Signal(name="attenuation", value=0.9)
 
 transmission = Signal(name="transmission", value=0.1)
