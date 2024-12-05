a6938972f103da5f9b8013dadcddc6cd4c9b29e9
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2024-12-05 12:30:57 +1100

    add default values for sim energy

--- mx3_beamline_library/devices/sim/optics.py
+++ mx3_beamline_library/devices/sim/optics.py
@@ -1,7 +1,7 @@
 from ophyd import Signal
 
-energy_master = Signal(name="energy_master")
+energy_master = Signal(name="energy_master", value=13.0)
 
-energy_dmm = Signal(name="energy_dmm")
+energy_dmm = Signal(name="energy_dmm", value=13.0)
 
-attenuation = Signal(name="attenuation")
+attenuation = Signal(name="attenuation", value=10.0)
