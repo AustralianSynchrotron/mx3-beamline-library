2e469cefb7b6f811bd06555905b9c1c2577c9d86
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2024-12-12 12:58:04 +1100

    update tests

--- tests/test_beam.py
+++ tests/test_beam.py
@@ -1,4 +1,10 @@
-from mx3_beamline_library.devices.beam import energy_master, transmission, attenuation, ring_current, energy_dmm
+from mx3_beamline_library.devices.beam import (
+    attenuation,
+    energy_dmm,
+    energy_master,
+    ring_current,
+    transmission,
+)
 
 
 def test_energy_master():
@@ -6,21 +12,24 @@ def test_energy_master():
     energy_master.set(13.2)
     assert energy_master.get() == 13.2
 
+
 def test_transmission():
     assert transmission.get() == 0.1
     transmission.set(0.2)
     assert transmission.get() == 0.2
+
 
 def test_attenuation():
     assert attenuation.get() == 0.9
     attenuation.set(0.8)
     assert attenuation.get() == 0.8
 
+
 def test_energy_dmm():
     assert energy_dmm.get() == 13.0
     energy_dmm.set(13.2)
     assert energy_dmm.get() == 13.2
 
+
 def test_ring_current():
     assert ring_current.get() == 200.0
-
--- tests/test_cryo.py
+++ tests/test_cryo.py
@@ -1,4 +1,5 @@
 from mx3_beamline_library.devices.cryo import cryo_temperature
 
+
 def test_cryo_temperature():
     assert cryo_temperature.get() == 270.0
