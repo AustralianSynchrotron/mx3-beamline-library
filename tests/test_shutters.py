e159e499694d086da0998344ae257e398071d429
Francisco Hernandez Vivanco <49014169+fhernandezvivanco@users.noreply.github.com>
2024-12-13 08:58:51 +1100

    Update shutter device (#52)
    * update default shutter sim value
    
    * update tests
    
    * update tests
    
    * pyproject.toml
    
    * adds pytest.ini

--- tests/test_shutters.py
+++ tests/test_shutters.py
@@ -1,11 +1,18 @@
-from mx3_beamline_library.devices.shutters import white_beam_shutter, mono_beam_shutter
+from mx3_beamline_library.devices.classes.shutters import OpenCloseCmd, OpenCloseStatus
+from mx3_beamline_library.devices.shutters import mono_beam_shutter, white_beam_shutter
+
 
 def test_white_beam_shutter():
     assert white_beam_shutter.open_close_status.get() == 2
+    assert white_beam_shutter.open_close_status.get() == OpenCloseStatus.CLOSED
     assert white_beam_shutter.open_enabled.get() == 1
-    white_beam_shutter.open_close_cmd.set(1) # close valve # TODO: test with real HW
+    white_beam_shutter.open_close_cmd.set(
+        OpenCloseCmd.CLOSE_VALVE
+    )  # TODO: test with real HW
+
 
 def test_mono_beam_shutter():
     assert mono_beam_shutter.open_close_status.get() == 2
+    assert white_beam_shutter.open_close_status.get() == OpenCloseStatus.CLOSED
     assert mono_beam_shutter.open_enabled.get() == 1
-    mono_beam_shutter.open_close_cmd.set(1) # close valve # TODO: test with real HW
+    mono_beam_shutter.open_close_cmd.set(1)  # TODO: test with real HW
