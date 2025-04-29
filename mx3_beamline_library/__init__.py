b82578c094e6d251a800979600e33b575e9eaefa
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-04-29 10:26:49 +1000

    add version

--- mx3_beamline_library/__init__.py
+++ mx3_beamline_library/__init__.py
@@ -1,1 +1,12 @@
-__version__ = "1.0.0"
+import tomllib
+import os
+
+
+def get_version():
+    pyproject_path = os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")
+    with open(pyproject_path, "rb") as f:
+        pyproject = tomllib.load(f)
+    return pyproject["project"]["version"]
+
+
+__version__ = get_version()
