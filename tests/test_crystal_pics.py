d44846a261ac716d9c353fa25439c35c3c47f42c
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-05-08 15:21:57 +1000

    update tests

--- tests/test_crystal_pics.py
+++ tests/test_crystal_pics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 from pytest_mock import MockerFixture
 
 from mx3_beamline_library.plans.crystal_pics import (
@@ -7,6 +8,7 @@ from mx3_beamline_library.plans.crystal_pics import (
     save_mxcube_grid_scan_crystal_pic,
     save_screen_or_dataset_crystal_pic_to_redis,
 )
+from mx3_beamline_library.plans.optical_centering import OpticalCentering
 
 
 def test_save_screen_crystal_pic_to_redis(fake_redis, mocker: MockerFixture):
@@ -140,7 +142,7 @@ def test_save_mxcube_grid_scan_crystal_pic(fake_redis, mocker: MockerFixture):
     assert isinstance(result, bytes)
 
 
-def test_get_grid_scan_crystal_pic(fake_redis, mocker: MockerFixture):
+def test_get_grid_scan_mxcube_crystal_pic(fake_redis, mocker: MockerFixture):
     # Setup
     mocker.patch("mx3_beamline_library.plans.crystal_pics.redis_connection", fake_redis)
     sample_id = "test_sample"
@@ -150,6 +152,39 @@ def test_get_grid_scan_crystal_pic(fake_redis, mocker: MockerFixture):
         sample_id,
         grid_scan_id,
     )
+
+    # Execute
+    result = get_grid_scan_crystal_pic(
+        sample_id,
+        grid_scan_id,
+    )
+
+    # Verify
+    assert result is not None
+    assert isinstance(result, np.ndarray)
+
+
+@pytest.mark.parametrize("grid_scan_id", ["flat", "edge"])
+def test_get_grid_scan_udc_crystal_pic(
+    run_engine, fake_redis, mocker: MockerFixture, sample_id, grid_scan_id
+):
+    # Setup
+    mocker.patch(
+        "mx3_beamline_library.plans.optical_centering.redis_connection", fake_redis
+    )
+    mocker.patch("mx3_beamline_library.plans.crystal_pics.redis_connection", fake_redis)
+
+    optical_centering = OpticalCentering(
+        sample_id=sample_id,
+        beam_position=(612, 512),
+        grid_step=(100, 100),
+        plot=False,
+        calibrated_alignment_z=0.47,
+        manual_mode=False,
+        use_top_camera_camera=True,
+    )
+
+    run_engine(optical_centering.center_loop())
 
     # Execute
     result = get_grid_scan_crystal_pic(
