2e76dc19171ad920fdb17e5cc0a97c3ab2bbdcd8
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-06-30 12:26:31 +1000

    wip

--- mx3_beamline_library/plans/manual_xray_centering.py
+++ mx3_beamline_library/plans/manual_xray_centering.py
@@ -176,16 +176,16 @@ class ManualXRayCentering(XRayCentering):
             )
             return
 
-        # Move the motors to the top left bottom coordinate position
-        # to ensure the grid is shown correctly in mxcube
-        yield from md3_move(
-            md3.sample_x,
-            grid.initial_pos_sample_x,
-            md3.sample_y,
-            grid.initial_pos_sample_y,
-            md3.alignment_y,
-            grid.initial_pos_alignment_y,
-        )
+        # # Move the motors to the top left bottom coordinate position
+        # # to ensure the grid is shown correctly in mxcube
+        # yield from md3_move(
+        #     md3.sample_x,
+        #     grid.initial_pos_sample_x,
+        #     md3.sample_y,
+        #     grid.initial_pos_sample_y,
+        #     md3.alignment_y,
+        #     grid.initial_pos_alignment_y,
+        # )
 
     def start_grid_scan(self) -> Generator[Msg, None, None]:
         """
