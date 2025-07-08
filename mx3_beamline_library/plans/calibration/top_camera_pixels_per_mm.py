ab2605f5c1fc1ad94f954e117ef406761ff3f560
Francisco Hernandez Vivanco <49014169+fhernandezvivanco@users.noreply.github.com>
2025-07-08 10:41:25 +1000

    Top camera calibration (#74)
    * get top camera pixels per mm
    
    * get top camera pixels per mm and target coords from redis
    
    * remove pixels per mm and target coords from schema
    
    * pre-commit
    
    * add docstrings
    
    * pre-commit
    
    * pre-commit
    
    * remove unused camera
    
    * add example
    
    * version bump

---
+++ mx3_beamline_library/plans/calibration/top_camera_pixels_per_mm.py
@@ -0,0 +1,230 @@
+from typing import Generator
+
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
+from bluesky.utils import Msg
+
+from mx3_beamline_library.config import redis_connection
+from mx3_beamline_library.devices import detectors
+from mx3_beamline_library.devices.motors import md3
+from mx3_beamline_library.logger import setup_logger
+from mx3_beamline_library.plans.image_analysis import get_image_from_top_camera
+from mx3_beamline_library.plans.plan_stubs import md3_move
+from mx3_beamline_library.schemas.optical_centering import OpticalCenteringExtraConfig
+from mx3_beamline_library.science.optical_and_loop_centering.loop_edge_detection import (
+    LoopEdgeDetection,
+)
+
+logger = setup_logger()
+
+config = OpticalCenteringExtraConfig()
+roi_x = config.top_camera.roi_x
+roi_y = config.top_camera.roi_y
+
+
+def _find_tip_of_loop(plot=False) -> npt.NDArray:
+    """
+    Finds the tip of the loop in the image taken by the top camera.
+
+    Parameters
+    ----------
+    plot : bool, optional
+        If True, the image with the detected tip will be plotted and saved.
+
+    Returns
+    -------
+    NDArray
+        The coordinates of the tip of the loop in the image.
+    """
+    img, height, width = get_image_from_top_camera(np.uint8)
+    img = img.reshape(height, width)
+    img = img[
+        roi_y[0] : roi_y[1],
+        roi_x[0] : roi_x[1],
+    ]
+
+    procImg = LoopEdgeDetection(img, block_size=49, adaptive_constant=6)
+    screen_coordinates = procImg.find_tip()
+    if plot:
+        plt.figure()
+        # plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+        plt.imshow(img)
+        plt.savefig("raw_data")
+        plt.close()
+        _save_image(img, screen_coordinates, filename="top_camera")
+
+    return screen_coordinates
+
+
+def _save_image(
+    data: npt.NDArray, screen_coordinates: npt.NDArray, filename: str
+) -> None:
+    """
+    Saves an image from a numpy array taken from the camera ophyd object,
+    and draws a red cross at the screen_coordinates.
+
+    Parameters
+    ----------
+    data : npt.NDArray
+        A numpy array containing an image from the camera
+    screen_coordinates : npt.NDArray
+        The coordinates of the tip of the loop in the image.
+    filename : str
+        The filename
+
+    Returns
+    -------
+    None
+    """
+    x_coord = screen_coordinates[0]
+    y_coord = screen_coordinates[1]
+    plt.figure()
+    plt.imshow(data, cmap="gray", vmin=0, vmax=255)
+    plt.scatter(
+        x_coord,
+        y_coord,
+        s=200,
+        c="r",
+        marker="+",
+    )
+    # plt.legend()
+    plt.savefig(filename)
+    plt.close()
+
+
+def _get_x_and_y_coords() -> tuple[float, float]:
+    """
+    Gets the x and y coordinates of the tip of the loop in the image taken by the top camera.
+    It takes 30 images and returns the median of the x and y coordinates.
+
+    Returns
+    -------
+    tuple[float, float]
+        The median x and y coordinates of the tip of the loop in the image.
+    """
+    camera = detectors.blackfly_camera
+    camera.wait_for_connection()
+
+    x_vals = []
+    y_vals = []
+    plt.figure()
+    for _ in range(30):
+        coords = _find_tip_of_loop()
+        x_vals.append(coords[0])
+        y_vals.append(coords[1])
+        plt.scatter(
+            coords[0],
+            coords[1],
+            s=200,
+            c="r",
+            marker="+",
+        )
+    y_median = np.median(y_vals)
+
+    x_median = np.median(x_vals)
+
+    return x_median, y_median
+
+
+def _get_pixels_per_mm_y() -> Generator[Msg, None, float]:
+    """
+    Calculates the number of pixels per mm in the y direction by moving the alignment_y motor
+    and measuring the change in pixel coordinates of the tip of the loop.
+
+    Returns
+    -------
+    Generator[Msg, None, float]
+        A generator that yields the number of pixels per mm in the y direction.
+    """
+
+    start_alignment_y = 0
+    start_sample_x = 0
+    start_sample_y = 0
+    start_omega = 0
+    start_alignment_z = 0
+    yield from md3_move(
+        md3.omega,
+        start_omega,
+        md3.alignment_y,
+        start_alignment_y,
+        md3.sample_x,
+        start_sample_x,
+        md3.sample_y,
+        start_sample_y,
+        md3.alignment_z,
+        start_alignment_z,
+    )
+    start_pixel_x, start_pixel_y = _get_x_and_y_coords()
+
+    end_alignment_y = 1
+    yield from md3_move(md3.alignment_y, end_alignment_y)
+    end_pixel_x, end_pixel_y = _get_x_and_y_coords()
+
+    pixels_per_mm_y = abs(start_pixel_y - end_pixel_y) / abs(
+        start_alignment_y - end_alignment_y
+    )
+    logger.info(f"Pixels per mm y: {pixels_per_mm_y}")
+    return pixels_per_mm_y
+
+
+def _get_pixels_per_mm_x() -> Generator[Msg, None, float]:
+    """
+    Calculates the number of pixels per mm in the x direction by moving the alignment_z motor
+    and measuring the change in pixel coordinates of the tip of the loop.
+
+    Returns
+    -------
+    Generator[Msg, None, float]
+        A generator that yields the number of pixels per mm in the x direction.
+    """
+    start_alignment_y = 0
+    start_sample_x = 0
+    start_sample_y = 0
+    start_omega = 0
+    start_alignment_z = 0
+    yield from md3_move(
+        md3.omega,
+        start_omega,
+        md3.alignment_y,
+        start_alignment_y,
+        md3.sample_x,
+        start_sample_x,
+        md3.sample_y,
+        start_sample_y,
+        md3.alignment_z,
+        start_alignment_z,
+    )
+    start_pixel_x, start_pixel_y = _get_x_and_y_coords()
+
+    end_alignment_z = 1
+    yield from md3_move(md3.alignment_z, end_alignment_z)
+    end_pixel_x, end_pixel_y = _get_x_and_y_coords()
+
+    pixels_per_mm_x = abs(start_pixel_x - end_pixel_x) / abs(
+        start_alignment_z - end_alignment_z
+    )
+    logger.info(f"Pixels per mm x: {pixels_per_mm_x}")
+    return pixels_per_mm_x
+
+
+def set_x_and_y_pixels_per_mm() -> Generator[Msg, None, None]:
+    """
+    This function calculates the number of pixels per mm in the x and y directions
+    by moving the alignment_y and alignment_z motors and measuring the change in pixel
+    coordinates of the tip of the loop. The results are stored in the redis database
+    under the key "top_camera_pixels_per_mm".
+
+    Returns
+    -------
+    Generator[Msg, None, None]
+    """
+    pixels_per_mm_x = yield from _get_pixels_per_mm_x()
+    pixels_per_mm_y = yield from _get_pixels_per_mm_y()
+    redis_connection.hset(
+        "top_camera_pixels_per_mm",
+        mapping={
+            "pixels_per_mm_x": float(pixels_per_mm_x),
+            "pixels_per_mm_y": float(pixels_per_mm_y),
+        },
+    )
---
+++ mx3_beamline_library/plans/calibration/top_camera_target_coords.py
@@ -0,0 +1,304 @@
+from typing import Generator
+
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
+from bluesky.plan_stubs import mv
+from bluesky.utils import Msg
+
+from ...config import BL_ACTIVE, redis_connection
+from ...devices.detectors import blackfly_camera
+from ...devices.motors import md3
+from ...logger import setup_logger
+from ...schemas.optical_centering import OpticalCenteringExtraConfig, TopCameraConfig
+from ...science.optical_and_loop_centering.loop_edge_detection import LoopEdgeDetection
+from ..image_analysis import get_image_from_top_camera
+from ..plan_stubs import md3_move
+
+logger = setup_logger()
+
+
+class TopCameraTargetCoords:
+    """
+    This class is used to set the top camera target coordinates in redis.
+    """
+
+    def __init__(
+        self,
+        plot: bool = False,
+        extra_config: OpticalCenteringExtraConfig | None = None,
+    ) -> None:
+        """
+        Parameters
+        ----------
+        plot : bool, optional
+            If True, the images taken by the top camera are plotted and saved, by default False
+        extra_config : OpticalCenteringExtraConfig | None, optional
+            The extra configuration values used during optical centering. If None, the default
+            value is set to OpticalCenteringExtraConfig()
+        """
+        self.top_camera = blackfly_camera
+        self.plot = plot
+
+        self._check_top_camera_config()
+        self._set_optical_centering_config_parameters(extra_config)
+
+    def _check_top_camera_config(self) -> None:
+        """
+        Checks that the top camera is configured correctly. If it is not,
+        PV values are updated following the TopCameraConfig model
+
+        Returns
+        -------
+        None
+        """
+        config = TopCameraConfig()
+        if BL_ACTIVE == "true":
+            if (
+                self.top_camera.cc1_enable_callbacks.get()
+                != config.cc1.enable_callbacks
+            ):
+                self.top_camera.cc1_enable_callbacks.set(config.cc1.enable_callbacks)
+
+            if self.top_camera.nd_array_port.get() != config.image.nd_array_port:
+                self.top_camera.nd_array_port.set(config.image.nd_array_port)
+
+            if self.top_camera.enable_callbacks.get() != config.cam.enable_callbacks:
+                self.top_camera.enable_callbacks.set(config.cam.enable_callbacks)
+
+            if self.top_camera.array_callbacks.get() != config.cam.array_callbacks:
+                self.top_camera.array_callbacks.set(config.cam.array_callbacks)
+
+            if self.top_camera.frame_rate_enable.get() != config.cam.frame_rate_enable:
+                self.top_camera.frame_rate_enable.set(config.cam.frame_rate_enable)
+
+            if self.top_camera.gain_auto.get() != config.cam.gain_auto:
+                self.top_camera.gain_auto.set(config.cam.gain_auto)
+
+            if self.top_camera.exposure_auto.get() != config.cam.exposure_auto:
+                self.top_camera.exposure_auto.set(config.cam.exposure_auto)
+
+            if self.top_camera.pixel_format.get() != config.cam.pixel_format:
+                self.top_camera.pixel_format.set(config.cam.pixel_format)
+
+            if round(self.top_camera.frame_rate.get(), 1) != config.cam.frame_rate:
+                self.top_camera.frame_rate.set(config.cam.frame_rate)
+
+            if round(self.top_camera.gain.get(), 1) != config.cam.gain:
+                self.top_camera.gain.set(config.cam.gain)
+
+            if (
+                round(self.top_camera.exposure_time.get(), 3)
+                != config.cam.exposure_time
+            ):
+                self.top_camera.exposure_time.set(config.cam.exposure_time)
+
+            if (
+                round(self.top_camera.acquire_period.get(), 2)
+                != config.cam.acquire_period
+            ):
+                self.top_camera.acquire_period.set(config.cam.acquire_period)
+
+    def _set_optical_centering_config_parameters(
+        self, optical_centering_config: OpticalCenteringExtraConfig | None
+    ) -> None:
+        """
+        Sets the extra configuration values used during optical centering
+
+        Parameters
+        ----------
+        optical_centering_config : OpticalCenteringExtraConfig | None, optional
+        The optical centering extra configuration. This contains configuration that
+        does not change often. If extra_config is None, the default value is set to
+        OpticalCenteringExtraConfig()
+
+        Returns
+        -------
+        None
+        """
+        if optical_centering_config is None:
+            optical_centering_config = OpticalCenteringExtraConfig()
+
+        self.top_cam_block_size = (
+            optical_centering_config.top_camera.loop_image_processing.block_size
+        )
+        self.top_cam_adaptive_constant = (
+            optical_centering_config.top_camera.loop_image_processing.adaptive_constant
+        )
+        self.top_camera_roi_x = optical_centering_config.top_camera.roi_x
+        self.top_camera_roi_y = optical_centering_config.top_camera.roi_y
+
+    def _find_zoom_0_maximum_area(self) -> Generator[Msg, None, tuple[float, float]]:
+        """
+        Finds the angle where the area of the loop is maximum.
+        This means that the tip of the loop at zoom level 0
+        is calculated more accurately
+
+        Yields
+        ------
+        Generator[Msg, None, NDArray]
+            a bluesky plan that returns the coordinates of the tip of the loop
+        """
+
+        initial_omega = md3.omega.position
+        omega_list = [initial_omega, initial_omega + 90]
+        area_list = []
+        tip_coordinates = []
+
+        for omega in omega_list:
+            if omega != initial_omega:
+                yield from mv(md3.omega, omega)
+
+            img, height, width = get_image_from_top_camera(np.uint8)
+            img = img.reshape(height, width)
+            img = img[
+                self.top_camera_roi_y[0] : self.top_camera_roi_y[1],
+                self.top_camera_roi_x[0] : self.top_camera_roi_x[1],
+            ]
+
+            edge_detection = LoopEdgeDetection(
+                img,
+                block_size=self.top_cam_block_size,
+                adaptive_constant=self.top_cam_adaptive_constant,
+            )
+            area_list.append(edge_detection.loop_area())
+            tip = edge_detection.find_tip()
+            tip_coordinates.append(tip)
+
+            if self.plot:
+                filename = f"top_camera_omega_{int(omega)}"
+                self.save_image(
+                    img,
+                    tip[0],
+                    tip[1],
+                    filename,
+                    grayscale_img=True,
+                )
+
+        argmax = np.argmax(area_list)
+        yield from mv(md3.omega, omega_list[argmax])
+
+        # average results for consistency
+        x_coord = [tip_coordinates[argmax][0]]
+        y_coord = [tip_coordinates[argmax][1]]
+        for _ in range(5):
+            img, height, width = get_image_from_top_camera(np.uint8)
+            img = img.reshape(height, width)
+            img = img[
+                self.top_camera_roi_y[0] : self.top_camera_roi_y[1],
+                self.top_camera_roi_x[0] : self.top_camera_roi_x[1],
+            ]
+            edge_detection = LoopEdgeDetection(
+                img,
+                block_size=self.top_cam_block_size,
+                adaptive_constant=self.top_cam_adaptive_constant,
+            )
+            tip = edge_detection.find_tip()
+            x_coord.append(tip[0])
+            y_coord.append(tip[1])
+        return (np.median(x_coord), np.median(y_coord))
+
+    def set_top_camera_target_coords(self) -> Generator[Msg, None, None]:
+        """
+        Sets the top camera target coordinates in redis for the top camera
+        under the key "top_camera_target_coords".
+
+        Yields
+        ------
+        Generator[Msg, None, None]
+            A bluesky generator
+        """
+        start_alignment_y = 0
+        start_sample_x = 0
+        start_sample_y = 0
+        start_omega = 0
+        start_alignment_z = 0
+        start_alignment_x = 0.434
+        yield from md3_move(
+            md3.omega,
+            start_omega,
+            md3.alignment_y,
+            start_alignment_y,
+            md3.sample_x,
+            start_sample_x,
+            md3.sample_y,
+            start_sample_y,
+            md3.alignment_z,
+            start_alignment_z,
+            md3.alignment_x,
+            start_alignment_x,
+        )
+
+        if md3.zoom.position != 1:
+            yield from mv(md3.zoom, 1)
+
+        if md3.zoom.get() != 1:
+            raise ValueError(
+                "The MD3 zoom could not be changed. Check the MD3 UI and try again."
+            )
+
+        if round(md3.backlight.get()) != 2:
+            yield from mv(md3.backlight, 2)
+
+        screen_coordinates = yield from self._find_zoom_0_maximum_area()
+
+        x_coord = screen_coordinates[0]
+        y_coord = screen_coordinates[1]
+
+        logger.info(f"x_pixel_target: {x_coord}")
+        logger.info(f"y_pixel_target: {y_coord}")
+
+        redis_connection.hset(
+            "top_camera_target_coords",
+            mapping={
+                "x_pixel_target": float(x_coord),
+                "y_pixel_target": float(y_coord),
+            },
+        )
+
+        return True
+
+    def save_image(
+        self,
+        data: npt.NDArray,
+        x_coord: float,
+        y_coord: float,
+        filename: str,
+        grayscale_img: bool = False,
+    ) -> None:
+        """
+        Saves an image from a numpy array taken from the camera ophyd object,
+        and draws a red cross at the screen_coordinates.
+
+        Parameters
+        ----------
+        data : npt.NDArray
+            A numpy array containing an image from the camera
+        x_coord : float
+            X coordinate
+        y_coord : float
+            Y coordinate
+        grayscale_img : bool
+            If the image is in grayscale, set this value to True, by default False
+        filename : str
+            The filename
+
+        Returns
+        -------
+        None
+        """
+        plt.figure()
+        if grayscale_img:
+            plt.imshow(data, cmap="gray", vmin=0, vmax=255)
+        else:
+            plt.imshow(data)
+        plt.scatter(
+            x_coord,
+            y_coord,
+            s=150,
+            c="r",
+            marker="x",
+        )
+        plt.title(f"omega={round(md3.omega.position)}", fontsize=18)
+        plt.savefig(filename, dpi=70)
+        plt.close()
