cd3a7068c8b8c7192f95773cd23a8bb0e2961812
Kate Smith <kates@ansto.gov.au>
2025-09-09 12:27:02 +1000

    individual telemetry and combined telemetry examples

--- examples/calibrate_top_camera.py
+++ examples/calibrate_top_camera.py
@@ -1,5 +1,9 @@
 from bluesky import RunEngine
 
+from opentelemetry.trace import get_tracer
+
+tracer = get_tracer(__name__)
+
 from mx3_beamline_library.plans.calibration import (
     top_camera_pixels_per_mm,
     top_camera_target_coords,
@@ -9,6 +13,14 @@ top_camera_coords = top_camera_target_coords.TopCameraTargetCoords()
 
 RE = RunEngine()
 
+# individual telemetry for each plan
 RE(top_camera_pixels_per_mm.set_x_and_y_pixels_per_mm())
-
 RE(top_camera_coords.set_top_camera_target_coords())
+
+# Or you can combine these together if you want combined telemetry
+def full_top_camera_calibration():
+    with tracer.start_as_current_span("full_top_camera_calibration"):
+        yield from top_camera_pixels_per_mm.set_x_and_y_pixels_per_mm()
+        yield from top_camera_coords.set_top_camera_target_coords()
+
+RE(full_top_camera_calibration())
