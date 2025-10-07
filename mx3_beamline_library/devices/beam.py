f588f021f4d19b44cc79a2aec2608d131e62627c
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-10-07 11:53:53 +1100

    add beam steering pvs

--- mx3_beamline_library/devices/beam.py
+++ mx3_beamline_library/devices/beam.py
@@ -35,3 +35,51 @@ ring_current = EpicsSignalRO(
 dmm_stripe = EpicsSignalRO("MX3MONO01:STRIPE_POSITION_MONITOR", name="dmm_stripe")
 
 flux = EpicsSignalRO("MX3FLUXIOC:FLUX", name="flux")
+
+# PVs used for beam steering
+ROOT = "MX3DAQIOC04:"
+# Change control. 1 is EPICS 0 is FPGA
+control = EpicsSignal(ROOT + "PreDAC0:OutMux", name="control")
+
+
+# syntax is "ON" or "OFF"
+steering_enable = EpicsSignal(ROOT + "PID:Enable", name="steering_enable")
+
+# Output channels. 1 is X and 2 is Y. Value is volts as float e.g. "1.2"
+x_Volt_SP = EpicsSignal(ROOT + "PreDAC0:OutCh2", name="x_Volt_SP")
+y_Volt_SP = EpicsSignal(ROOT + "PreDAC0:OutCh1", name="y_Volt_SP")
+
+# voltage readbacks (volts)
+x_Volt_RBV = EpicsSignal(ROOT + "PreDAC0:Ch2_RBV", name="x_Volt_RBV")
+y_Volt_RBV = EpicsSignal(ROOT + "PreDAC0:Ch1_RBV", name="y_Volt_RBV")
+
+# Flux readback (Amps)
+
+# Setpoints
+x_SP = EpicsSignal(ROOT + "PID:SetpointX", name="x_SP")
+y_SP = EpicsSignal(ROOT + "PID:SetpointY", name="y_SP")
+
+# Position Readback
+x_RBV = EpicsSignal(ROOT + "BPM0:PosX_RBV", name="x_RBV")
+y_RBV = EpicsSignal(ROOT + "BPM0:PosY_RBV", name="y_RBV")
+
+# flux_readback
+flux_beam_steering = EpicsSignal(ROOT + "BPM0:Int_RBV", name="flux")
+
+# BeamOffThreshold
+beamOffThreshold_SP = EpicsSignal(ROOT + "BPM0:BeamOffTh", name="beamOffThreshold_SP")
+beamOffThreshold_RBV = EpicsSignal(
+    ROOT + "BPM0:BeamOffTh_RBV", name="beamOffThreshold_RBV"
+)
+
+kill_goni_lateral = EpicsSignal(
+    "MX3STG02MOT01:STOP_KILL.PROC", name="kill_goni_lateral"
+)
+kill_goni_vertical = EpicsSignal(
+    "MX3STG02MOT02:STOP_KILL.PROC", name="kill_goni_vertical"
+)
+
+
+# Position Readback
+x_RBV = EpicsSignal(ROOT + "BPM0:PosX_RBV", name="x_RBV")
+y_RBV = EpicsSignal(ROOT + "BPM0:PosY_RBV", name="y_RBV")
--- mx3_beamline_library/devices/sim/beam.py
+++ mx3_beamline_library/devices/sim/beam.py
@@ -15,3 +15,43 @@ ring_current = Signal(name="ring_current", value=200.0)
 dmm_stripe = Signal(name="dmm_stripe", value=1)
 
 flux = Signal(name="flux", value=2.30e11)
+
+# PVs used for beam steering
+control = Signal(name="control", value=0)
+
+
+# syntax is "ON" or "OFF"
+steering_enable = Signal(name="steering_enable", value=0)
+
+# Output channels. 1 is X and 2 is Y. Value is volts as float e.g. "1.2"
+x_Volt_SP = Signal(name="x_Volt_SP", value=0)
+y_Volt_SP = Signal(name="y_Volt_SP", value=0)
+
+# voltage readbacks (volts)
+x_Volt_RBV = Signal(name="x_Volt_RBV", value=0)
+y_Volt_RBV = Signal(name="y_Volt_RBV", value=0)
+
+# Flux readback (Amps)
+
+# Setpoints
+x_SP = Signal(name="x_SP", value=0)
+y_SP = Signal(name="y_SP", value=0)
+
+# Position Readback
+x_RBV = Signal(name="x_RBV", value=0)
+y_RBV = Signal(name="y_RBV", value=0)
+
+# flux_readback
+flux_beam_steering = Signal(name="flux_beam_steering", value=0)
+
+# BeamOffThreshold
+beamOffThreshold_SP = Signal(name="beamOffThreshold_SP", value=0)
+beamOffThreshold_RBV = Signal(name="beamOffThreshold_RBV", value=0)
+
+kill_goni_lateral = Signal(name="kill_goni_lateral", value=0)
+kill_goni_vertical = Signal(name="kill_goni_vertical", value=0)
+
+
+# Position Readback
+x_RBV = Signal(name="x_RBV", value=0)
+y_RBV = Signal(name="y_RBV", value=0)
